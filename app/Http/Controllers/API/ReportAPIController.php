bd113c3efe37a125bc81eba7f2c845e54d268d9c
Mubashir <mubashir@gears-int.com>
2018-04-12 11:01:06 +0530

    GWL-38 added po analyis report

--- app/Http/Controllers/API/ReportAPIController.php
+++ app/Http/Controllers/API/ReportAPIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\AppBaseController;
 use App\Models\ProcumentOrder;
+use App\Models\PurchaseOrderDetails;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -26,24 +27,37 @@ class ReportAPIController extends AppBaseController
 
                 $suppliers = (array)$request->suppliers;
                 $suppliers = collect($suppliers)->pluck('supplierCodeSytem');
-                $output = ProcumentOrder::with(['detail' => function ($query) use ($companyID) {
-                    $query->with(['financecategory' => function ($query) {
-                        $query->select('itemCategoryID', 'categoryDescription');
-                    }, 'financecategorysub' => function ($query) {
-                        $query->with(['finance_gl_code_pl' => function ($query) {
-                            $query->select('chartOfAccountSystemID', 'AccountDescription','AccountCode');
-                        }]);
-                        $query->select('itemCategorySubID', 'categoryDescription', 'financeGLcodePLSystemID');
-                    },'unit' => function ($query){
-                        $query->select('UnitID', 'UnitShortCode');
+
+                $output = PurchaseOrderDetails::whereHas('order',function ($query) use ($companyID, $startDate, $endDate){
+                    $query->whereBetween('approvedDate', [$startDate, $endDate])
+                        ->where('approved', -1)
+                        ->whereIN('companySystemID', $companyID);
+                })->with(['order' => function ($query) use ($companyID, $startDate, $endDate) {
+                    $query->with(['segment' => function ($query) {
+                        $query->select('ServiceLineDes', 'serviceLineSystemID');
+                    }, 'location' => function ($query) {
+                        $query->select('locationName', 'locationID');
                     }]);
-                    $query->leftJoin('erp_grvdetails as gdet',function($join) use ($companyID){
-                        $join->on('erp_purchaseorderdetails.purchaseOrderDetailsID','=','gdet.purchaseOrderDetailsID');
-                        $join->whereIN('gdet.companySystemID',$companyID);
-                        $join->selectRaw('SUM(gdet.noQty) as noQty');
-                    });
-                    $query->leftJoin(
-                        DB::raw('(SELECT
+                    $query->selectRaw('purchaseOrderID,companyID,poLocation,serviceLineSystemID,approved,companyID,YEAR ( approvedDate ) AS PostingYear,approvedDate AS OrderDate,purchaseOrderCode,IF
+	( sentToSupplier = 0, "Not Released", "Released" ) AS STATUS,supplierID,supplierPrimaryCode,supplierName,creditPeriod,deliveryTerms,paymentTerms,expectedDeliveryDate,narration')
+                        ->whereBetween('approvedDate', [$startDate, $endDate])
+                        ->where('approved', -1)
+                        ->whereIN('companySystemID', $companyID);
+                }, 'financecategory' => function ($query) {
+                    $query->select('itemCategoryID', 'categoryDescription');
+                }, 'financecategorysub' => function ($query) {
+                    $query->with(['finance_gl_code_pl' => function ($query) {
+                        $query->select('chartOfAccountSystemID', 'AccountDescription', 'AccountCode');
+                    }]);
+                    $query->select('itemCategorySubID', 'categoryDescription', 'financeGLcodePLSystemID');
+                }, 'unit' => function ($query) {
+                    $query->select('UnitID', 'UnitShortCode');
+                }])->leftJoin('erp_grvdetails as gdet', function ($join) use ($companyID) {
+                    $join->on('erp_purchaseorderdetails.purchaseOrderDetailsID', '=', 'gdet.purchaseOrderDetailsID');
+                    $join->whereIN('gdet.companySystemID', $companyID);
+                    $join->selectRaw('SUM(gdet.noQty) as noQty');
+                })->leftJoin(
+                    DB::raw('(SELECT
     erp_grvdetails.purchaseOrderDetailsID,
     erp_grvdetails.purchaseOrderMastertID,
     erp_grvdetails.itemCode,
@@ -54,63 +68,53 @@ FROM
     ) 
 WHERE
     erp_grvmaster.approved=- 1 and purchaseOrderDetailsID>0 GROUP BY erp_grvdetails.purchaseOrderMastertID,erp_grvdetails.purchaseOrderDetailsID,erp_grvdetails.itemCode) as gdet2'),
-                        function($join) use ($companyID)
-                        {
-                            $join->on('erp_purchaseorderdetails.purchaseOrderMasterID','=','gdet2.purchaseOrderMastertID');
-                            $join->on('erp_purchaseorderdetails.purchaseOrderDetailsID','=','gdet2.purchaseOrderDetailsID');
-                            $join->on('erp_purchaseorderdetails.itemCode','=','gdet2.itemCode');
-                        });
-
-                    $query->selectRaw('gdet2.lastOfgrvDate,
+                    function ($join) use ($companyID) {
+                        $join->on('erp_purchaseorderdetails.purchaseOrderMasterID', '=', 'gdet2.purchaseOrderMastertID');
+                        $join->on('erp_purchaseorderdetails.purchaseOrderDetailsID', '=', 'gdet2.purchaseOrderDetailsID');
+                        $join->on('erp_purchaseorderdetails.itemCode', '=', 'gdet2.itemCode');
+                    })->selectRaw('erp_purchaseorderdetails.purchaseOrderMasterID,erp_purchaseorderdetails.purchaseOrderDetailsID,gdet2.lastOfgrvDate,
                     erp_purchaseorderdetails.unitOfMeasure,
                     IF((erp_purchaseorderdetails.noQty-gdet.noQty) = 0,"Fully Received",if(ISNULL(gdet.noQty) OR gdet.noQty=0 ,"Not Recieved","Partially Recieved")) as receivedStatus,
-                    IFNULL((erp_purchaseorderdetails.noQty-gdet.noQty),0) as qtyToReceive,IFNULL(gdet.noQty,0) as qtyReceived,erp_purchaseorderdetails.itemFinanceCategoryID,erp_purchaseorderdetails.itemFinanceCategorySubID,erp_purchaseorderdetails.purchaseOrderDetailsID,erp_purchaseorderdetails.purchaseOrderMasterID,erp_purchaseorderdetails.itemPrimaryCode,erp_purchaseorderdetails.itemDescription,erp_purchaseorderdetails.supplierPartNumber,erp_purchaseorderdetails.noQty,( ( erp_purchaseorderdetails.GRVcostPerUnitComRptCur / ( 100- erp_purchaseorderdetails.discountPercentage ) ) * 100 ) AS UnitCostWithOutDiscount,erp_purchaseorderdetails.GRVcostPerUnitComRptCur as UnitCostWithDiscount,erp_purchaseorderdetails.discountPercentage,( ( ( ( erp_purchaseorderdetails.GRVcostPerUnitComRptCur / ( 100- erp_purchaseorderdetails.discountPercentage ) ) * 100 ) ) - erp_purchaseorderdetails.GRVcostPerUnitComRptCur ) AS discountAmount,( erp_purchaseorderdetails.noQty * erp_purchaseorderdetails.GRVcostPerUnitComRptCur ) AS Total');
-                }, 'segment' => function ($query) {
-                    $query->select('ServiceLineDes', 'serviceLineSystemID');
-                }, 'location' => function ($query) {
-                    $query->select('locationName', 'locationID');
-                }])->selectRaw('companyID,poLocation,serviceLineSystemID,purchaseOrderID,approved,companyID,YEAR ( approvedDate ) AS PostingYear,approvedDate AS OrderDate,purchaseOrderCode,IF
-	( sentToSupplier = 0, "Not Released", "Released" ) AS STATUS,supplierID,supplierPrimaryCode,supplierName,creditPeriod,deliveryTerms,paymentTerms,expectedDeliveryDate,narration')->whereBetween('approvedDate', [$startDate, $endDate])->where('approved', -1)->whereIN('companySystemID', $companyID)->get();
-
+                    IFNULL((erp_purchaseorderdetails.noQty-gdet.noQty),0) as qtyToReceive,IFNULL(gdet.noQty,0) as qtyReceived,erp_purchaseorderdetails.itemFinanceCategoryID,erp_purchaseorderdetails.itemFinanceCategorySubID,erp_purchaseorderdetails.itemPrimaryCode,erp_purchaseorderdetails.itemDescription,erp_purchaseorderdetails.supplierPartNumber,erp_purchaseorderdetails.noQty,( ( erp_purchaseorderdetails.GRVcostPerUnitComRptCur / ( 100- erp_purchaseorderdetails.discountPercentage ) ) * 100 ) AS UnitCostWithOutDiscount,erp_purchaseorderdetails.GRVcostPerUnitComRptCur as UnitCostWithDiscount,erp_purchaseorderdetails.discountPercentage,( ( ( ( erp_purchaseorderdetails.GRVcostPerUnitComRptCur / ( 100- erp_purchaseorderdetails.discountPercentage ) ) * 100 ) ) - erp_purchaseorderdetails.GRVcostPerUnitComRptCur ) AS discountAmount,( erp_purchaseorderdetails.noQty * erp_purchaseorderdetails.GRVcostPerUnitComRptCur ) AS Total')
+                    ->whereIN('erp_purchaseorderdetails.companySystemID', $companyID)->get();
                 $outputArray = [];
                 if ($output) {
                     foreach ($output as $key => $val) {
                         $outputArray[] = array(
-                            'companyID' => $val->companyID,
-                            'purchaseOrderCode' => $val->purchaseOrderCode,
-                            'postingYear' => $val->PostingYear,
-                            'orderDate' => $val->OrderDate,
-                            'STATUS' => $val->STATUS,
-                            'supplierPrimaryCode' => $val->supplierPrimaryCode,
-                            'supplierName' => $val->supplierName,
-                            'creditPeriod' => $val->creditPeriod,
-                            'deliveryTerms' => $val->deliveryTerms,
-                            'paymentTerms' => $val->paymentTerms,
-                            'expectedDeliveryDate' => $val->expectedDeliveryDate,
-                            'narration' => $val->narration,
-                            'total' => isset($val->detail)? $val->detail->Total: '',
-                            'lastOfgrvDate' => isset($val->detail)? $val->detail->lastOfgrvDate: '',
-                            'unitCostWithDiscount' => isset($val->detail) ? $val->detail->UnitCostWithDiscount: '',
-                            'unitCostWithOutDiscount' => isset($val->detail) ? $val->detail->UnitCostWithOutDiscount: '',
-                            'qtyReceived' => isset($val->detail) ? $val->detail->qtyReceived: '',
-                            'qtyToReceive' => isset($val->detail) ? $val->detail->qtyToReceive: '',
-                            'discountAmount' => isset($val->detail) ? $val->detail->discountAmount: '',
-                            'discountPercentage' => isset($val->detail) ? $val->detail->discountPercentage: '',
-                            'unitShortCode' => isset($val->detail) ? isset($val->detail->unit) ? $val->detail->unit->UnitShortCode : '': '',
-                            'financecategory' => isset($val->detail) ? isset($val->detail->financecategory) ? $val->detail->financecategory->categoryDescription : '': '',
-                            'financecategorysub' => isset($val->detail) ? isset($val->detail->financecategorysub) ? $val->detail->financecategorysub->categoryDescription : '': '',
-                            'finance_gl_code_pl' => isset($val->detail) ? isset($val->detail->financecategorysub) ? isset ($val->detail->financecategorysub->finance_gl_code_pl) ? $val->detail->financecategorysub->finance_gl_code_pl->AccountDescription : '' : '': '',
-                            'AccountCode' => isset($val->detail) ? isset($val->detail->financecategorysub) ? isset ($val->detail->financecategorysub->finance_gl_code_pl) ? $val->detail->financecategorysub->finance_gl_code_pl->AccountCode : '' : '': '',
-                            'itemDescription' => isset($val->detail) ?$val->detail->itemDescription: '',
-                            'itemPrimaryCode' => isset($val->detail) ?$val->detail->itemPrimaryCode: '',
-                            'noQty' => isset($val->detail) ? $val->detail->noQty: '',
-                            'receivedStatus' => isset($val->detail) ? $val->detail->receivedStatus: '',
-                            'receivedDate' => isset($val->detail) ? $val->detail->lastOfgrvDate: '',
-                            'location' => isset($val->location) ? $val->location->locationName : '',
-                            'segment' => isset($val->segment) ? $val->segment->ServiceLineDes : '');
+                            'companyID' => $val->order->companyID,
+                            'purchaseOrderCode' => $val->order->purchaseOrderCode,
+                            'postingYear' => $val->order->PostingYear,
+                            'orderDate' => $val->order->OrderDate,
+                            'STATUS' => $val->order->STATUS,
+                            'supplierPrimaryCode' => $val->order->supplierPrimaryCode,
+                            'supplierName' => $val->order->supplierName,
+                            'creditPeriod' => $val->order->creditPeriod,
+                            'deliveryTerms' => $val->order->deliveryTerms,
+                            'paymentTerms' => $val->order->paymentTerms,
+                            'expectedDeliveryDate' => $val->order->expectedDeliveryDate,
+                            'narration' => $val->order->narration,
+                            'total' => $val->Total,
+                            'lastOfgrvDate' => $val->lastOfgrvDate,
+                            'unitCostWithDiscount' => $val->UnitCostWithDiscount,
+                            'unitCostWithOutDiscount' => $val->UnitCostWithOutDiscount,
+                            'qtyReceived' => $val->qtyReceived,
+                            'qtyToReceive' => $val->qtyToReceive,
+                            'discountAmount' => $val->discountAmount,
+                            'discountPercentage' => $val->discountPercentage,
+                            'unitShortCode' => isset($val->unit) ? $val->unit->UnitShortCode : '',
+                            'financecategory' => isset($val->financecategory) ? $val->financecategory->categoryDescription : '',
+                            'financecategorysub' => isset($val->financecategory) ? $val->financecategorysub->categoryDescription : '',
+                            'finance_gl_code_pl' => isset ($val->financecategorysub) ? isset ($val->financecategorysub->finance_gl_code_pl) ? $val->financecategorysub->finance_gl_code_pl->AccountDescription : '' : '',
+                            'AccountCode' => isset ($val->financecategorysub->finance_gl_code_pl) ? $val->financecategorysub->finance_gl_code_pl->AccountCode : '',
+                            'itemDescription' => $val->itemDescription,
+                            'itemPrimaryCode' => $val->itemPrimaryCode,
+                            'noQty' => $val->noQty,
+                            'receivedStatus' => $val->receivedStatus,
+                            'receivedDate' => $val->lastOfgrvDate,
+                            'location' => isset($val->order->location) ? $val->order->location->locationName : '',
+                            'segment' => isset($val->order->segment) ? $val->order->segment->ServiceLineDes : '');
                     }
                 }
-
                 return $this->sendResponse($outputArray, 'Successfully report retrieved');
             default:
                 'Error Message';
