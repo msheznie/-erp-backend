e5ab2b268e7207b824ca09280ecb281c4ef689a7
Fayas <fayas@gears-int.com>
2018-05-08 18:09:37 +0530

    GWL-64 GWL-67 GWL-58 done

--- routes/api.php
+++ routes/api.php
@@ -387,6 +387,7 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     Route::resource('addresses', 'AddressAPIController');
     Route::post('getAllAddresses', 'AddressAPIController@getAllAddresses');
+    Route::get('getAddressFormData', 'AddressAPIController@getAddressFormData');
 
     Route::resource('address_types', 'AddressTypeAPIController');
 });
