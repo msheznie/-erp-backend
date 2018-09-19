bd912920014eaa1e67ace52351fb62162cce9dbf
Fayas <fayas@gears-int.com>
2018-09-19 15:05:30 +0530

    GWL-212 details table added

--- app/Http/Controllers/API/BankLedgerAPIController.php
+++ app/Http/Controllers/API/BankLedgerAPIController.php
@@ -8,7 +8,7 @@
  * -- Create date : 18 - September 2018
  * -- Description : This file contains the all CRUD for Bank Ledger
  * -- REVISION HISTORY
- * -- Date: 18-September 2018 By: Fayas Description: Added new functions named as
+ * -- Date: 19-September 2018 By: Fayas Description: Added new functions named as getBankReconciliationsByType()
  */
 namespace App\Http\Controllers\API;
 
@@ -232,7 +232,27 @@ class BankLedgerAPIController extends AppBaseController
             return $this->sendError('Bank Ledger not found');
         }
 
-        $bankLedger = $this->bankLedgerRepository->update($input, $id);
+        $employee = \Helper::getEmployeeInfo();
+        $updateArray = array();
+        $updateArray['trsClearedYN'] = $input['trsClearedYN'];
+
+        if($updateArray['trsClearedYN']){
+            $updateArray['trsClearedAmount'] = $bankLedger->payAmountSuppTrans;
+            $updateArray['trsClearedByEmpName'] = $employee->empName;
+            $updateArray['trsClearedByEmpID'] = $employee->empID;
+            $updateArray['trsClearedByEmpSystemID'] = $employee->employeeSystemID;
+            $updateArray['trsClearedDate'] = now();
+            $updateArray['bankRecAutoID'] = $input['bankRecAutoID'];
+        }else{
+            $updateArray['trsClearedAmount'] = 0;
+            $updateArray['trsClearedByEmpName'] = null;
+            $updateArray['trsClearedByEmpID'] = null;
+            $updateArray['trsClearedByEmpSystemID'] = null;
+            $updateArray['trsClearedDate'] = null;
+            $updateArray['bankRecAutoID'] = null;
+        }
+
+        $bankLedger = $this->bankLedgerRepository->update($updateArray, $id);
 
         return $this->sendResponse($bankLedger->toArray(), 'BankLedger updated successfully');
     }
@@ -288,4 +308,54 @@ class BankLedgerAPIController extends AppBaseController
 
         return $this->sendResponse($id, 'Bank Ledger deleted successfully');
     }
+
+    public function getBankReconciliationsByType(Request $request)
+    {
+        $input = $request->all();
+        $input = $this->convertArrayToSelectedValue($input, array('month', 'year'));
+
+        if (request()->has('order') && $input['order'][0]['column'] == 0 && $input['order'][0]['dir'] === 'asc') {
+            $sort = 'asc';
+        } else {
+            $sort = 'desc';
+        }
+
+        $selectedCompanyId = $request['companyId'];
+        $isGroup = \Helper::checkIsCompanyGroup($selectedCompanyId);
+
+        if ($isGroup) {
+            $subCompanies = \Helper::getGroupCompany($selectedCompanyId);
+        } else {
+            $subCompanies = [$selectedCompanyId];
+        }
+
+        $logistics = BankLedger::whereIn('companySystemID', $subCompanies)
+                                 ->where('invoiceType',$input['invoiceType']);
+                                //->where("bankAccountAutoID",$input['bankAccountAutoID'])
+                                //->with(['month','created_by','bank_account']);
+
+        $search = $request->input('search.value');
+
+        if ($search) {
+            $search = str_replace("\\", "\\\\", $search);
+            $logistics = $logistics->where(function ($query) use ($search) {
+                $query->where('documentCode', 'LIKE', "%{$search}%")
+                    ->orWhere('documentNarration', 'LIKE', "%{$search}%");
+            });
+        }
+
+        return \DataTables::eloquent($logistics)
+            ->addColumn('Actions', 'Actions', "Actions")
+            ->order(function ($query) use ($input) {
+                if (request()->has('order')) {
+                    if ($input['order'][0]['column'] == 0) {
+                        $query->orderBy('bankLedgerAutoID', $input['order'][0]['dir']);
+                    }
+                }
+            })
+            ->addIndexColumn()
+            ->with('orderCondition', $sort)
+            ->make(true);
+    }
+
 }
