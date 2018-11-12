cdcf3260f274389fa5b302630591fb5a56cddbe6
Fayas <fayas@gears-int.com>
2018-11-12 11:43:01 +0530

    GWL-296 issues fixed;

--- app/Models/HRMSDepartmentMaster.php
+++ app/Models/HRMSDepartmentMaster.php
@@ -66,7 +66,7 @@ class HRMSDepartmentMaster extends Model
         'DepartmentDescription',
         'isActive',
         'ServiceLineCode',
-        'CompanyID',
+        'CompanyID', 
         'showInCombo',
         'timestamp'
     ];
