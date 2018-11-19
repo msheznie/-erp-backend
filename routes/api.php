5abb68438b9f271b051d9118b1fa65627c486d3e
Mubashir <mubashir@gears-int.com>
2018-11-19 16:57:39 +0530

    GWL-200 when approving payment voucher or receipt voucher update bank ledger if the trsCollectedYN =0 in payment table or receipt table it should be updated as -1 is done, financial period update when receipt voucher created in fund transfer and change supplier name and code to payee is done

--- routes/api.php
+++ routes/api.php
@@ -1138,7 +1138,7 @@ Route::get('getBcryptPassword/{password}', function ($password) {
 });
 
 Route::get('runQueue', function () {
-    //$master = ['documentSystemID' => 21,'autoID' => 109, 'companySystemID' => 11, 'employeeSystemID' => 2664];
+    //$master = ['documentSystemID' => 4,'autoID' => 76750, 'companySystemID' => 11, 'employeeSystemID' => 2664];
     //$job = \App\Jobs\GeneralLedgerInsert::dispatch($master);
     //$master = \App\Models\PaySupplierInvoiceMaster::find(76750);
     //$job = \App\Jobs\CreateReceiptVoucher::dispatch($master);
