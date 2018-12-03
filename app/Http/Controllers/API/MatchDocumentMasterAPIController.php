2b3ecf04fab24098c29dc1914d38d72572e0618f
Nazir <nazir@gears-int.com>
2018-12-03 16:22:52 +0530

    GWL-267 Receipt matching searching filtration added

--- app/Http/Controllers/API/MatchDocumentMasterAPIController.php
+++ app/Http/Controllers/API/MatchDocumentMasterAPIController.php
@@ -1407,8 +1407,10 @@ WHERE
             $invMaster = $invMaster->where(function ($query) use ($search) {
                 $query->where('matchingDocCode', 'LIKE', "%{$search}%")
                     ->orWhere('BPVNarration', 'LIKE', "%{$search}%")
+                    ->orWhere('BPVcode', 'LIKE', "%{$search}%")
                     ->orWhereHas('customer', function ($query) use ($search) {
-                        $query->where('CustomerName', 'like', "%{$search}%");
+                        $query->where('CutomerCode', 'LIKE', "%{$search}%")
+                            ->orWhere('CustomerName', 'LIKE', "%{$search}%");
                     });
             });
         }
@@ -1433,6 +1435,10 @@ WHERE
 
         if (!isset($input['matchType'])) {
             return $this->sendError('Please select a match type');
+        }
+
+        if (!isset($input['BPVsupplierID'])) {
+            return $this->sendError('Please select a customer');
         }
 
         if ($input['matchType'] == 1) {
