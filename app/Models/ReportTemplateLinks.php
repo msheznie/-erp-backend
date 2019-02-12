22965a63f8ef5833ae22f6cce4ce22cbbeb0a6f4
Mubashir <mubashir@gears-int.com>
2019-02-12 17:17:51 +0530

    GWL-363 assign employees to report is done

--- app/Models/ReportTemplateLinks.php
+++ app/Models/ReportTemplateLinks.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * =============================================
+ * -- File Name : ReportTemplateLinks.php
+ * -- Project Name : ERP
+ * -- Module Name : Configuration
+ * -- Author : Mohamed Mubashir
+ * -- Create date : 30- January 2019
+ * -- Description : This file is used to interact with database table and it contains relationships to the tables.
+ * -- REVISION HISTORY
+ */
 namespace App\Models;
 
 use Eloquent as Model;
