dcd1f3e758ac8932b98b4597ca78a00fe5662141
Mubashir <mubashir@gears-int.com>
2019-04-10 13:03:10 +0530

    GWL-531 remove login check expiry is done

--- app/Listeners/RevokeOldTokens.php
+++ app/Listeners/RevokeOldTokens.php
@@ -33,11 +33,11 @@ class RevokeOldTokens
     public function handle(AccessTokenCreated $event)
     {
 
-      DB::table('oauth_access_tokens')
+      /*DB::table('oauth_access_tokens')
             ->where('id', '<>', $event->tokenId)
             ->where('user_id', $event->userId)
             ->where('client_id', $event->clientId)
-            ->update(['revoked' => true]);
+            ->update(['revoked' => true]);*/
 
        if(!empty($event->tokenId)){
             $logHistory = new UsersLogHistory();
