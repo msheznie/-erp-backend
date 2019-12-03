841b1953bf361977f7765781ecffe7ae4d6df084
rilwanmsm <rilwan.msm@gears-int.com>
2019-12-03 17:32:09 +0530

    GWL-710 - Payment voucher - Bank Reconciliation status

--- app/Http/Controllers/API/PaySupplierInvoiceMasterAPIController.php
+++ app/Http/Controllers/API/PaySupplierInvoiceMasterAPIController.php
@@ -1101,14 +1101,17 @@ function getPaymentVoucherMaster(Request $request)
     $output = PaySupplierInvoiceMaster::where('PayMasterAutoId', $input['PayMasterAutoId'])
         ->with(['supplier', 'bankaccount', 'transactioncurrency', 'supplierdetail',
             'company', 'localcurrency', 'rptcurrency', 'advancedetail', 'confirmed_by',
-            'modified_by', 'cheque_treasury_by', 'directdetail' => function ($query) {
+            'modified_by', 'cheque_treasury_by', 'directdetail' => function ($query) use($input) {
                 $query->with('segment');
             }, 'approved_by' => function ($query) {
                 $query->with('employee');
                 $query->where('documentSystemID', 4);
-            }, 'created_by', 'cancelled_by', 'bankledger_by' => function ($query) {
-                $query->with(['bankrec_by', 'bank_transfer']);
+            }, 'created_by', 'cancelled_by', 'bankledger_by' => function ($query) use($input){
                 $query->where('documentSystemID', 4);
+                $query->where('bankAccountID', function ($query) use($input){
+                    $query->select('BPVAccount')->from('erp_paysupplierinvoicemaster')->where('PayMasterAutoId',$input['PayMasterAutoId']);
+                });
+                $query->with(['bankrec_by', 'bank_transfer']);
             }])->first();
 
     return $this->sendResponse($output, 'Data retrieved successfully');
