a88eb32306164318f7b652256c50a88d4fbdc947
Mohammed Zakeeul <zakeeulsm@gmail.com>
2020-04-16 08:59:13 +0530

    GWL-829 companies alphabetically ordered in finance report

--- app/Http/Controllers/API/FinancialReportAPIController.php
+++ app/Http/Controllers/API/FinancialReportAPIController.php
@@ -733,7 +733,7 @@ class FinancialReportAPIController extends AppBaseController
         }
 
         $headers = collect($finalHeaders)->forget($removedFromArray)->values();
-
+        $companyHeaderData = collect($companyHeaderData)->sortBy('companyCode')->toArray();
         $companyHeaderColumns = [];
         foreach ($companyHeaderData as $key => $value) {
             $companyHeaderColumns[] = $value;
