4ef29b54ad16837d7b72a91bf034b4dc47f9c9c4
rilwanmsm <rilwan.msm@gears-int.com>
2020-08-18 11:28:18 +0530

    tb unmatch query changed

--- app/Http/Controllers/API/FinancialReportAPIController.php
+++ app/Http/Controllers/API/FinancialReportAPIController.php
@@ -4413,7 +4413,7 @@ GROUP BY
         $unmatchedData = GeneralLedger::selectRaw('documentCode, round( sum( erp_generalledger.documentLocalAmount ), 3 ), round( sum( erp_generalledger.documentRptAmount ), 2 ), documentSystemCode, documentSystemID')
                                       ->where('companySystemID', $input['companySystemID'])
                                       ->whereDate('documentDate','<=', $toDate)
-                                      ->groupBy('companySystemID', 'documentSystemCode')
+                                      ->groupBy('companySystemID', 'documentSystemCode','documentSystemID')
                                       ->havingRaw('round( sum( erp_generalledger.documentRptAmount ), 2 ) != 0 OR round( sum( erp_generalledger.documentLocalAmount ), 3 ) != 0')
                                       ->get();
 
