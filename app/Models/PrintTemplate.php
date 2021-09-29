1eb9617395993a2047f222f5c99ce0c34d060580
lahiru-dilshan <lahiru.dilshan@gears-int.com>
2021-09-29 17:46:59 +0530

    fixed issue

--- app/Models/PrintTemplate.php
+++ app/Models/PrintTemplate.php
@@ -80,9 +80,6 @@ class PrintTemplate extends Model
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
 
-
-
-
     public $fillable = [
         'document_id',
         'template_name',
