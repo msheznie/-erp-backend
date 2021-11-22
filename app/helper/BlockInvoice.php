469c085f98cc8db9c71a7ef882a21db4c634aff0
Arshad <m.arshad@gears-int.com>
2021-11-22 18:35:50 +0530

    MSG-122 Fixed issue delivery order blocking

--- app/helper/BlockInvoice.php
+++ app/helper/BlockInvoice.php
@@ -11,22 +11,26 @@ class BlockInvoice
 {
 	public static function blockCustomerInvoiceByCreditLimit($documentSystemID, $masterRecord)
 	{
-		
-		if (isset($masterRecord->customerID) && $masterRecord->customerID > 0 && $masterRecord->isPerforma != 1) {
-			$customerData = CustomerMaster::find($masterRecord->customerID);
+		if ((isset($masterRecord->customerID) && $masterRecord->customerID > 0 && $masterRecord->isPerforma != 1 && $documentSystemID == 20) || isset($masterRecord->customerID) && $masterRecord->customerID > 0 && $documentSystemID == 71) {
+			 $customerData = CustomerMaster::find($masterRecord->customerID);
 
 			if (!$customerData) {
 				return ['status' => false, 'message' => "Customer not found"];
 			}
-
+			
 			if ($customerData->creditLimit > 0 && $customerData->custGLAccountSystemID != null) {
 				$customerOutsanding = GeneralLedger::where('companySystemID', $masterRecord->companySystemID)
 							                        ->whereIn('documentSystemID', [20, 19, 21, 71])
 							                        ->where('supplierCodeSystem', $masterRecord->customerID)
 							                        ->where('chartOfAccountSystemID', $customerData->custGLAccountSystemID)
 							                        ->sum('documentRptAmount');
-
-				$customerNewOutsanding = floatval($masterRecord->bookingAmountRpt) +  $customerOutsanding;
+				if($masterRecord->documentSystemID == 20){
+					$amountRpt = $masterRecord->bookingAmountRpt;
+				} 
+				elseif($masterRecord->documentSystemID == 71){
+					$amountRpt = $masterRecord->companyReportingAmount;
+				}
+				$customerNewOutsanding = floatval($amountRpt) +  $customerOutsanding;
 
 				if ($customerNewOutsanding > 0 && ($customerNewOutsanding > $customerData->creditLimit)) {
 					$reportCurrencyDecimalPlace = 2;
