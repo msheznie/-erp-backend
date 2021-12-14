ee8e7295b94a253c0b1385878587f759120bea43
nilaf246 <nilaf.ahamed@gears-int.com>
2021-12-14 12:10:50 +0530

    MSG-187 remove currecy convertion

--- app/Http/Controllers/API/PaySupplierInvoiceMasterAPIController.php
+++ app/Http/Controllers/API/PaySupplierInvoiceMasterAPIController.php
@@ -1292,9 +1292,9 @@ class PaySupplierInvoiceMasterAPIController extends AppBaseController
     
                     $bankBalance = app('App\Http\Controllers\API\BankAccountAPIController')->getBankAccountBalanceSummery($cur_det_info);
     
-                    $bankBalance_amount = $bankBalance['netBankBalance'];
-                    $details = \Helper::currencyConversion($input['companySystemID'],$bank_currency,$document_currency, $bankBalance_amount,$input['BPVAccount']);
-                    $amount = $details['documentAmount'];
+                    $amount = $bankBalance['netBankBalance'];
+                    // $details = \Helper::currencyConversion($input['companySystemID'],$bank_currency,$document_currency, $bankBalance_amount,$input['BPVAccount']);
+                    // $amount = $details['documentAmount'];
                     $currencies = CurrencyMaster::where('currencyID','=',$document_currency)->select('DecimalPlaces')->first();
     
                     $rounded_amount =  number_format($amount,$currencies->DecimalPlaces,'.', '');
