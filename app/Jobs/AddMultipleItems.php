76e5171f7b7101966430cabc78cb69a370d9482f
ThatSaravanan1112 <thatparanthan.saravanan@gears-int.com>
2021-12-16 00:48:41 +0530

    cahage

--- app/Jobs/AddMultipleItems.php
+++ app/Jobs/AddMultipleItems.php
@@ -14,7 +14,7 @@ use App\Models\PurchaseOrderDetails;
 use App\Models\ItemMaster;
 use App\Models\ItemAssigned;
 use App\Models\ProcumentOrder;
-use App\helper\CommonJobService;
+use App\helper\ProcumentOrderService;
 
 
 class AddMultipleItems implements ShouldQueue
@@ -23,6 +23,7 @@ class AddMultipleItems implements ShouldQueue
 
     public $record;
     public $purchaseOrder;
+    public $timeout = 500;
     public $db;
     /**
      * Create a new job instance.
@@ -42,7 +43,6 @@ class AddMultipleItems implements ShouldQueue
         $this->record = $record;
         $this->purchaseOrder = $purchaseOrder;
         $this->db = $db;
-        CommonJobService::db_switch($db);
 
     }
 
@@ -53,51 +53,9 @@ class AddMultipleItems implements ShouldQueue
      */
     public function handle()
     {
-        Log::info('Add Mutiple Items Started');
-
-        Log::info('DB Name'.$this->db);
-        $items = $this->record;
-        $valiatedItems = $this->validateItem($items);
-
-        $procumentOrder = ProcumentOrder::find($this->purchaseOrder['purchaseOrderID']);
-        $procumentOrder->upload_job_status = 0;
-        $procumentOrder->save();
-        $procumentOrderDetails = PurchaseOrderDetails::insert($valiatedItems);
-        Log::info('Add Mutiple Items End');
-        $procumentOrder = ProcumentOrder::find($this->purchaseOrder['purchaseOrderID']);
-        $procumentOrder->upload_job_status = 1;
-        $procumentOrder->save();
-
+        ProcumentOrderService::addMultipleItems($this->record,$this->purchaseOrder, $this->db);
     }
 
 
-    public function validateItem($items) {
-        $validatedItemsArray = [];
-        foreach($items as $item) {
-            if(array_key_exists('item_code',$item)) {
-                $orgItem = ItemMaster::where('primaryCode',trim($item['item_code']))->first();
-                if($orgItem) {
-                    $item['purchaseOrderMasterID'] = $this->purchaseOrder['purchaseOrderID'];
-                    $item['companyID'] = $this->purchaseOrder['companyID'];
-                    $item['companySystemID'] = $this->purchaseOrder['companySystemID'];
-                    $item['serviceLineSystemID'] = $this->purchaseOrder['serviceLineSystemID'];
-                    $item['serviceLineCode'] = $this->purchaseOrder['serviceLine'];
-                    $item['itemCode'] = $orgItem['itemCodeSystem'];
-                    $item['unitCost'] = trim($item['unit_cost']);
-                    $item['noQty'] = trim($item['no_qty']);
-                    $item['itemPrimaryCode'] = trim($orgItem['primaryCode']);
-                    $item['itemDescription'] = trim($orgItem['itemDescription']);
-                    $item['netAmount'] =   $item['unitCost'] * $item['noQty'];
-                    $item['itemCode'] = $item['item_code'];
-                    unset($item['item_code'], $item['unit_cost'], $item['no_qty']);
-                    $item['unitOfMeasure'] = trim($orgItem['unit']);
-                    $item['altUnit'] = trim($orgItem['unit']);
-                    $item['altUnitValue'] = trim($item['noQty']);
-                    array_push($validatedItemsArray,$item);
-                }
-            }
-        }
-
-        return $validatedItemsArray;
-    }
+   
 }
