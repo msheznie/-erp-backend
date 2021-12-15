fe7bd13b9bce49f92764ccd3810ed01fa9bd5bde
ThatSaravanan1112 <thatparanthan.saravanan@gears-int.com>
2021-12-16 00:10:19 +0530

    changes

--- app/helper/CommonJobService.php
+++ app/helper/CommonJobService.php
@@ -6,14 +6,17 @@ use App\Models\Company;
 use App\Models\Tenant;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class CommonJobService
 {
     public static function db_switch( $db ){
         if($db)
         {
+            Log::info("database Name in common service -".$db);
             Config::set("database.connections.mysql.database", $db);
-            DB::reconnect('mysql');
+            // DB::reconnect('mysql');
+            DB::purge('mysql');
         }
 
 
