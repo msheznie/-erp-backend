ad5f5d1ad3e29b665c9a6d6c140782bd332d8775
ThatSaravanan1112 <thatparanthan.saravanan@gears-int.com>
2021-12-17 11:25:14 +0530

    budget related bug fixes

--- app/helper/general_helper.php
+++ app/helper/general_helper.php
@@ -224,6 +224,7 @@ class Helper
       
 
             $docInforArr = array('documentCodeColumnName' => '', 'confirmColumnName' => '', 'confirmedBy' => '', 'confirmedBySystemID' => '', 'confirmedDate' => '', 'tableName' => '', 'modelName' => '', 'primarykey' => '');
+         
             switch ($params["document"]) { // check the document id and set relavant parameters
                 case 1:
                 case 50:
@@ -658,6 +659,7 @@ class Helper
                     $docInforArr["tableName"] = 'erp_budgetaddition';
                     $docInforArr["modelName"] = 'ErpBudgetAddition';
                     $docInforArr["primarykey"] = 'id';
+                    break;
                 case 104:
                     $docInforArr["documentCodeColumnName"] = 'returnFillingCode';
                     $docInforArr["confirmColumnName"] = 'confirmedYN';
