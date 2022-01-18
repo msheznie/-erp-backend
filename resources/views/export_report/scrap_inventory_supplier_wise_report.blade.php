595120cdb7c6a5fd09da7d2a6a96938c2cd08d45
YasasaR <yasasa.jayasuriya@gears-int.com>
2022-01-18 18:41:48 +0530

    MSG-244 currency issue fixed

--- resources/views/export_report/scrap_inventory_supplier_wise_report.blade.php
+++ resources/views/export_report/scrap_inventory_supplier_wise_report.blade.php
@@ -122,24 +122,24 @@ foreach($grvItems as $grvItem){
                     <td>{{ $grvItem->wasteQty }}</td>
                     <td>{{ $grvItem->noQty }}</td>
                     @if($currency_id == 1)
-                        <td>{{ number_format($grvItem->unitCost,$company->localcurrency->DecimalPlaces) }}</td>
+                        <td>{{ number_format($grvItem->unitCost / $grvItem->localCurrencyER,$company->localcurrency->DecimalPlaces) }}</td>
 
                     @endif
 
                     @if($currency_id == 2)
-                        <td>{{ number_format($grvItem->unitCost,$company->reportingcurrency->DecimalPlaces) }}</td>
+                        <td>{{ number_format($grvItem->unitCost / $grvItem->companyReportingER,$company->reportingcurrency->DecimalPlaces) }}</td>
 
                     @endif
 
                     @if($currency_id == 1)
 
-                        <td>{{ number_format($grvItem->netAmount,$company->localcurrency->DecimalPlaces) }}</td>
+                        <td>{{ number_format($grvItem->netAmount / $grvItem->localCurrencyER,$company->localcurrency->DecimalPlaces) }}</td>
 
                     @endif
 
                     @if($currency_id == 2)
 
-                        <td>{{ number_format($grvItem->netAmount,$company->reportingcurrency->DecimalPlaces) }}</td>
+                        <td>{{ number_format($grvItem->netAmount / $grvItem->companyReportingER,$company->reportingcurrency->DecimalPlaces) }}</td>
 
                     @endif
                     <td>{{ $grvItem->comment }}</td>
