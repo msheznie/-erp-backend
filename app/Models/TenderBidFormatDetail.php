27d0adc36826fa629e3d6e0e715183959aa7c657
Mushtaq Ahamed <mushtaq@gears-int.com>
2022-03-04 18:21:28 +0530

    GSUP-395

---
+++ app/Models/TenderBidFormatDetail.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+/**
+ * @SWG\Definition(
+ *      definition="TenderBidFormatDetail",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="tender_id",
+ *          description="tender_id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="label",
+ *          description="label",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="field_type",
+ *          description="field_type",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="is_disabled",
+ *          description="is_disabled",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_at",
+ *          description="created_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_by",
+ *          description="created_by",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_at",
+ *          description="updated_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_by",
+ *          description="updated_by",
+ *          type="integer",
+ *          format="int32"
+ *      )
+ * )
+ */
+class TenderBidFormatDetail extends Model
+{
+
+    public $table = 'tender_bid_format_detail';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+
+
+    public $fillable = [
+        'tender_id',
+        'label',
+        'field_type',
+        'is_disabled',
+        'created_by',
+        'updated_by'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'tender_id' => 'integer',
+        'label' => 'string',
+        'field_type' => 'integer',
+        'is_disabled' => 'integer',
+        'created_by' => 'integer',
+        'updated_by' => 'integer'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
