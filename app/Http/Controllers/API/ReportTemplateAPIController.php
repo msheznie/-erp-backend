43c238f2cccc6fc30bda99036ae2aad09791e392
Yasasa <yasasa.jayasuriya@gears-int.com>
2022-04-06 15:15:07 +0530

    PGUTEC-1401 checked condition for $isDefault

--- app/Http/Controllers/API/ReportTemplateAPIController.php
+++ app/Http/Controllers/API/ReportTemplateAPIController.php
@@ -333,26 +333,28 @@ class ReportTemplateAPIController extends AppBaseController
 
             if (isset($input['reportID']) && isset($input['companyReportTemplateID'])) {
                 $isDefault = ReportTemplate::find($input['companyReportTemplateID']);
-                if ($input['isDefault'] != $isDefault->isDefault) {
-                if ($input['reportID'] == 1 || $input['reportID'] == 2) {
-                    $templates = ReportTemplate::with(['details' => function ($query) {
-                        $query->with(['gllink']);
-                    }])->where('reportID', $input['reportID'])->where('isDefault', 1)->get();
-                    $isCOA = false;
-                    foreach ($templates as $template) {
-                        foreach ($template->details as $detail) {
-                            foreach ($detail->gllink as $gllink) {
-                                $isCOA = true;
+                if($isDefault) {
+                    if ($input['isDefault'] != $isDefault->isDefault) {
+                        if ($input['reportID'] == 1 || $input['reportID'] == 2) {
+                            $templates = ReportTemplate::with(['details' => function ($query) {
+                                $query->with(['gllink']);
+                            }])->where('reportID', $input['reportID'])->where('isDefault', 1)->get();
+                            $isCOA = false;
+                            foreach ($templates as $template) {
+                                foreach ($template->details as $detail) {
+                                    foreach ($detail->gllink as $gllink) {
+                                        $isCOA = true;
+                                    }
+                                }
+                            }
+
+                            if ($isCOA == true) {
+                                return $this->sendError('Cannot change default report template because chart of account is created already', 500);
                             }
                         }
                     }
-
-                    if ($isCOA == true) {
-                        return $this->sendError('Cannot change default report template because chart of account is created already', 500);
-                    }
                 }
             }
-        }
 
         $reportTemplate = $this->reportTemplateRepository->update($input, $id);
 
