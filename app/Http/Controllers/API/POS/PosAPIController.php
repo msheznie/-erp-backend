7e0912976969ad6cea36a45b4320627e88b623e5
nilaf246 <nilaf.ahamed@gears-int.com>
2022-06-13 11:04:21 +0530

    GCP-150 pos api unit of measure

--- app/Http/Controllers/API/POS/PosAPIController.php
+++ app/Http/Controllers/API/POS/PosAPIController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\API\POS;
 
 use App\Http\Controllers\AppBaseController;
 use App\Models\CustomerMasterCategory;
-use App\models\ErpLocation;
+use App\Models\ErpLocation;
 use Illuminate\Support\Facades\DB;
 use App\Models\SegmentMaster;
+use App\Models\Unit;
+use App\Models\UnitConversion;
 
 class PosAPIController extends AppBaseController
 {
@@ -43,21 +45,53 @@ class PosAPIController extends AppBaseController
             return $this->sendError($exception->getMessage());
         }
    }
- public function pullSegment()
- {
-    DB::beginTransaction();
-    try {
-        $segments = SegmentMaster::selectRaw('serviceLineSystemID As id,ServiceLineCode As segment_code ,ServiceLineDes as description,isActive as status')
-        ->where('ServiceLineCode','!=','')
-        ->where('ServiceLineDes','!=','')
-        ->get();
-        DB::commit();
-        return $this->sendResponse($segments, 'Data Retrieved successfully');
-    } catch (\Exception $exception) {
-        DB::rollBack();
-        return $this->sendError($exception->getMessage());
+    public function pullSegment()
+    {
+        DB::beginTransaction();
+        try {
+            $segments = SegmentMaster::selectRaw('serviceLineSystemID As id,ServiceLineCode As segment_code ,ServiceLineDes as description,isActive as status')
+            ->where('ServiceLineCode','!=','')
+            ->where('ServiceLineDes','!=','')
+            ->get();
+            DB::commit();
+            return $this->sendResponse($segments, 'Data Retrieved successfully');
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return $this->sendError($exception->getMessage());
+        }
+
     }
 
- }
+    public function pullUnitOfMeasure()
+    {
+        DB::beginTransaction();
+        try {
+            $units = Unit::selectRaw('UnitID As id,UnitShortCode As short_code ,UnitDes as description')
+            ->where('UnitShortCode','!=','')
+            ->get();
+            DB::commit();
+            return $this->sendResponse($units, 'Data Retrieved successfully');
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return $this->sendError($exception->getMessage());
+        }
+    }
+
+    public function pullUnitConversion()
+    {
+        DB::beginTransaction();
+        try {
+            $unitConvertion = UnitConversion::selectRaw('unitsConversionAutoID As id,masterUnitID As master_id ,subUnitID as sub_id,conversion as conversion')
+            ->where('masterUnitID','!=','')
+            ->where('subUnitID','!=','')
+            ->where('conversion','!=','')
+            ->get();
+            DB::commit();
+            return $this->sendResponse($unitConvertion, 'Data Retrieved successfully');
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            return $this->sendError($exception->getMessage());
+        }
+    }
 
 }
