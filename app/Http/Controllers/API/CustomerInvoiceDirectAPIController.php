6f59347585e318ea032801ee27c53227efd82163
Arshad <m.arshad@gears-int.com>
2022-06-16 18:36:00 +0530

    PGUTEC-1628 Fixed issues in customer invoice

--- app/Http/Controllers/API/CustomerInvoiceDirectAPIController.php
+++ app/Http/Controllers/API/CustomerInvoiceDirectAPIController.php
@@ -371,6 +371,23 @@ class CustomerInvoiceDirectAPIController extends AppBaseController
             return $this->sendError('Customer Invoice Direct not found');
         }
 
+         
+        $customerInvoiceLogisticData = [
+            'consignee_address'=>$customerInvoiceDirect->customer->consignee_address, 
+            'consignee_contact_no'=>$customerInvoiceDirect->customer->consignee_contact_no, 
+            'consignee_name'=>$customerInvoiceDirect->customer->consignee_name, 
+            'payment_terms'=>$customerInvoiceDirect->customer->payment_terms,
+            'custInvoiceDirectAutoID'=>$id
+        ];
+
+        $invoiceLogistic = CustomerInvoiceLogistic::where('custInvoiceDirectAutoID',$id)->first();
+        if($invoiceLogistic){
+            $customerInvoiceLogistic = CustomerInvoiceLogistic::where('id', $invoiceLogistic['id'])->update($customerInvoiceLogisticData);
+        } else {
+            $customerInvoiceLogistic = CustomerInvoiceLogistic::create($customerInvoiceLogisticData);
+        }
+
+
 
         return $this->sendResponse($customerInvoiceDirect->toArray(), 'Customer Invoice Direct retrieved successfully');
     }
