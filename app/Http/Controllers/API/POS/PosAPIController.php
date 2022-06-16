5ddc68a9e9852058cf293676e872066740b0e18c
nilaf246 <nilaf.ahamed@gears-int.com>
2022-06-16 17:33:26 +0530

    GCP-151 pos api chart of account

--- app/Http/Controllers/API/POS/PosAPIController.php
+++ app/Http/Controllers/API/POS/PosAPIController.php
@@ -61,7 +61,13 @@ class PosAPIController extends AppBaseController
     {
         DB::beginTransaction();
         try {
-            $chartOfAccount = ChartOfAccount::selectRaw('chartOfAccountSystemID As id,AccountCode As system_code')
+            $chartOfAccount = ChartOfAccount::selectRaw('chartOfAccountSystemID As id,AccountCode As system_code,AccountCode As secondary_code,AccountDescription as description,
+            isMasterAccount as is_master_account,masterAccount as master_account_id , "" as master_system_code,catogaryBLorPL as master_category,
+            "" as category_id,"" as category_description,"" as sub_category,controllAccountYN as is_control_account,isActive as is_active,"" as default_type,
+            "" as is_auto,"" as is_card,isBank as is_bank,"" as is_cash,"" as is_default_bank,"" as bank_name,"" as bank_branch,"" as bank_short_code,"" as bank_swift_code,"" as bank_cheque_number,
+            "" as bank_account_number, "" as bank_currency_id,"" as bank_currency_code, "" as bank_currency_decimal,"" as is_deleted,"" as deleted_userID,"" as deleted_dateTime,
+            confirmedYN as confirmedYN,"" as confirmedDate,confirmedEmpID as confirmedbyEmpID,confirmedEmpName as confirmedbyName,isApproved as approvedYN,approvedDate as approvedDate,
+            approvedBySystemID as approvedbyEmpID,approvedBy as approvedbyEmpName,approvedComment as approvedComment')
             ->where('AccountCode','!=','')
             ->get();
     
