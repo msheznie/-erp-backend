e94ab250d8e9749f27ed039721d869a8d21f70ce
Yasasa <yasasa.jayasuriya@gears-int.com>
2022-06-22 20:13:21 +0530

    OCC-131 adjustments on top and bottom padding

--- resources/views/print/payment_voucher.blade.php
+++ resources/views/print/payment_voucher.blade.php
@@ -575,7 +575,7 @@
             </table>
         </div>
     @endif
-    <div style="padding-bottom: 5px!important; padding-top: 5px!important;">
+    <div style="padding-bottom: 15px!important; padding-top: 5px!important;">
     <table style="width:100%;">
         <tr>
             <td width="40%"><span
