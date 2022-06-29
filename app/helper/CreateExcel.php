d8aee78578e9b88f734a1632eef49f9bfd9b23d9
nilaf246 <nilaf.ahamed@gears-int.com>
2022-06-29 17:14:32 +0530

    OCC-153 expoert excel details fetching  Account payable

--- app/helper/CreateExcel.php
+++ app/helper/CreateExcel.php
@@ -9,18 +9,42 @@ use Illuminate\Support\Facades\Storage;
 class CreateExcel
 {
 
-    public static function process($data,$type,$fileName,$path_dir)
+    public static function process($data,$type,$fileName,$path_dir,$from_date = NULL,$to_date = NULL,$company_name = NULL)
     {
 
-        $excel_content =  \Excel::create('payment_suppliers_by_year', function ($excel) use ($data,$fileName) {
-            $excel->sheet($fileName, function ($sheet) use ($data) {
-                $sheet->fromArray($data, null, 'A1', true);
+        $excel_content =  \Excel::create('payment_suppliers_by_year', function ($excel) use ($data,$fileName,$company_name,$from_date,$to_date) {
+            $excel->sheet($fileName, function ($sheet) use ($data,$fileName,$company_name,$from_date,$to_date) {
+                $sheet->cell('D1', function($cell) use($fileName,$company_name) 
+                {
+                    $cell->setValue($fileName);  
+                    $cell->setFontSize(16);
+                    $cell->setAlignment('center');
+                });
+                $sheet->cell('D2', function($cell) use($fileName,$company_name) 
+                {
+                    $cell->setValue($company_name);  
+                    $cell->setFontSize(16);
+                    $cell->setAlignment('center');
+                });
+
+                $sheet->cell('A3', function($cell) use($fileName,$from_date) 
+                {
+                    $cell->setValue('From Date - '.$from_date);  
+                    $cell->setFontSize(12);
+                });
+                $sheet->cell('A4', function($cell) use($fileName,$to_date) 
+                {
+                    $cell->setValue('To Date - '.$to_date);  
+                    $cell->setFontSize(12);
+                });
+
+                $sheet->fromArray($data, null, 'A7', true);
                 $sheet->setAutoSize(true);
                 $sheet->getStyle('C1:C2')->getAlignment()->setWrapText(true);
             });
 
             $lastrow = $excel->getActiveSheet()->getHighestRow();
-            $excel->getActiveSheet()->getStyle('A1:J' . $lastrow)->getAlignment()->setWrapText(true);
+            //$excel->getActiveSheet()->getStyle('A1:J' . $lastrow)->getAlignment()->setWrapText(true);
         })->string($type);
 
         $disk = 's3';
