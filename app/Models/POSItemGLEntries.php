6aa6b140fc1e47e7426b6974669b05805c99acc9
Yasasa <yasasa.jayasuriya@gears-int.com>
2022-08-14 02:27:22 +0530

    GCP-203 models created for newly created tables

---
+++ app/Models/POSItemGLEntries.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+/**
+ * @SWG\Definition(
+ *      definition="POSItemGLEntries",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="shiftId",
+ *          description="shiftId",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="itemAutoId",
+ *          description="itemAutoId",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="uom",
+ *          description="uom",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="qty",
+ *          description="qty",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="isReturnYN",
+ *          description="isReturnYN",
+ *          type="integer",
+ *          format="int32"
+ *      )
+ * )
+ */
+class POSItemGLEntries extends Model
+{
+
+    public $table = 'pos_item_gl';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+
+
+    public $fillable = [
+        'shiftId',
+        'itemAutoId',
+        'uom',
+        'qty',
+        'isReturnYN'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'shiftId' => 'integer',
+        'itemAutoId' => 'integer',
+        'uom' => 'integer',
+        'qty' => 'integer',
+        'isReturnYN' => 'integer'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
