483a33513d941ce54663ddf456c06e20f6a4b3e4
Zakeeul <shakeeul@osos.om>
2022-09-06 12:28:17 +0530

    PGUTEC-1818 budget cutoff notification job

--- app/Services/BudgetCutOffNotificationService.php
+++ app/Services/BudgetCutOffNotificationService.php
@@ -58,12 +58,12 @@ class BudgetCutOffNotificationService
         $x = 1;
         foreach ($details as $val) {
             $body .= '<tr>
-                <td style="text-align:left;border: 1px solid black;">' . $val->documentCode . '</td> 
-                <td style="text-align:left;border: 1px solid black;">' . $val->segment . '</td> 
-                <td style="text-align:left;border: 1px solid black;">' . $val->currency . '</td> 
-                <td style="text-align:right;border: 1px solid black;">' .$val->documentValue . '</td> 
-                <td style="text-align:right;border: 1px solid black;">' .$val->remainingValue . '</td> 
-                <td style="text-align:center;border: 1px solid black;">' . $val->cutOffDate . '</td> 
+                <td style="text-align:left;border: 1px solid black;">' . $val['documentCode'] . '</td> 
+                <td style="text-align:left;border: 1px solid black;">' . $val['segment'] . '</td> 
+                <td style="text-align:left;border: 1px solid black;">' . $val['currency'] . '</td> 
+                <td style="text-align:right;border: 1px solid black;">' .$val['documentValue'] . '</td> 
+                <td style="text-align:right;border: 1px solid black;">' .$val['remainingValue'] . '</td> 
+                <td style="text-align:center;border: 1px solid black;">' . $val['cutOffDate'] . '</td> 
                 </tr>';
             $x++;
         }
@@ -72,16 +72,83 @@ class BudgetCutOffNotificationService
         return $body;
     }
 
-    public static function getCutOffPurchaseOrders($dataBase, $partiallyRecivedPos, $type, $days, $emails, $companyIDFromScenarios)
+    public static function getCutOffPurchaseOrders($dataBase, $partiallyRecivedPos, $type, $days, $emails, $companyIDFromScenario)
     {
 
-        $jobHeaderData['poCount'] = count($partiallyRecivedPos);
-        $jobHeaderData['jobCount'] = 0;
+        // $jobHeaderData['poCount'] = count($partiallyRecivedPos);
+        // $jobHeaderData['jobCount'] = 0;
 
-        PoCutoffJob::create($jobHeaderData);
-
+        // PoCutoffJob::create($jobHeaderData);
+        $poData = [];
         foreach ($partiallyRecivedPos as $key => $value) {
-           PurchaseOrderCutOffCheckJob::dispatch($dataBase, $type, $days, $value, $emails, $companyIDFromScenarios);
+           // PurchaseOrderCutOffCheckJob::dispatch($dataBase, $type, $days, $value, $emails, $companyIDFromScenario);
+
+            $now = Carbon::now();
+            $checkBudget = CompanyPolicyMaster::where('companyPolicyCategoryID', 17)
+                                        ->where('companySystemID', $value['companySystemID'])
+                                        ->first();
+
+            if ($checkBudget && $checkBudget->isYesNO) {
+                $budgetConsumedData = BudgetConsumptionService::getBudgetIdsByConsumption($value['documentSystemID'], $value['purchaseOrderID']);
+
+                if (count($budgetConsumedData['budgetmasterIDs']) > 0) {
+                    $budgetIds = array_unique($budgetConsumedData['budgetmasterIDs']);
+
+                    foreach ($budgetIds as $key1 => $value1) {
+                        $budgetMaster = BudgetMaster::with(['finance_year_by', 'segment_by'])->find($value1);
+
+                        if ($budgetMaster && $budgetMaster->finance_year_by) {
+                            $cutOffDate = Carbon::parse($budgetMaster->finance_year_by->endingDate)->addMonths($budgetMaster->cutOffPeriod);
+
+                            $diff = $now->diffInDays($cutOffDate);
+                            $temp = [];
+                            $temp['documentCode'] = $value['purchaseOrderCode'];
+                            $temp['segment'] = ($budgetMaster && $budgetMaster->segment_by) ? $budgetMaster->segment_by->ServiceLineCode." - ".$budgetMaster->segment_by->ServiceLineDes : "";
+                            $temp['currency'] = ($value['currency']) ? $value['currency']['CurrencyCode'] : "";
+                            $temp['documentValue'] = ($value['currency']) ? number_format($value['poTotalSupplierTransactionCurrency'], $value['currency']['DecimalPlaces']) : number_format($value['poTotalSupplierTransactionCurrency'], 2);
+                            $temp['cutOffDate'] = $cutOffDate->format('Y-m-d');
+
+                            if (($cutOffDate > $now) && $diff > 0) {
+                                if ($type == 1 && ($diff == $days)) { // Before 
+                                   $recivedValue = ($value['grvRecieved'] == 1)  ? GRVDetails::selectRaw('SUM(erp_grvdetails.GRVcostPerUnitSupTransCur*erp_grvdetails.noQty) as total')->where('purchaseOrderMastertID', $value['purchaseOrderID'])->first()->total : 0;
+                                   $temp['remainingValue'] = ($value['currency']) ? number_format(($value['poTotalSupplierTransactionCurrency'] - $recivedValue), $value['currency']['DecimalPlaces']) : number_format(($value['poTotalSupplierTransactionCurrency'] - $recivedValue), 2);
+
+                                   // PoCutoffJobData::create($temp);
+                                   $poData[] = $temp;
+                                } 
+                            } else if ($cutOffDate < $now && $diff > 0) {
+                                if ($type == 2 && ($diff == $days)) { // After
+                                   $recivedValue = ($value['grvRecieved'] == 1)  ? GRVDetails::selectRaw('SUM(erp_grvdetails.GRVcostPerUnitSupTransCur*erp_grvdetails.noQty) as total')->where('purchaseOrderMastertID', $value['purchaseOrderID'])->first()->total : 0;
+                                   $temp['remainingValue'] = ($value['currency']) ? number_format(($value['poTotalSupplierTransactionCurrency'] - $recivedValue), $value['currency']['DecimalPlaces']) : number_format(($value['poTotalSupplierTransactionCurrency'] - $recivedValue), 2);
+                                   // PoCutoffJobData::create($temp);
+                                   $poData[] = $temp;
+                                }
+                            } else {
+                                if ($type == 0 && ($diff == 0)) { // Same Day
+                                   $recivedValue = ($value['grvRecieved'] == 1)  ? GRVDetails::selectRaw('SUM(erp_grvdetails.GRVcostPerUnitSupTransCur*erp_grvdetails.noQty) as total')->where('purchaseOrderMastertID', $value['purchaseOrderID'])->first()->total : 0;
+                                   $temp['remainingValue'] = ($value['currency']) ? number_format(($value['poTotalSupplierTransactionCurrency'] - $recivedValue), $value['currency']['DecimalPlaces']) : number_format(($value['poTotalSupplierTransactionCurrency'] - $recivedValue), 2);
+                                   // PoCutoffJobData::create($temp);
+                                   $poData[] = $temp;
+                                } 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        if (count($poData) > 0) {
+            $subject = "Open Purchase Order/s Reaching budget cutoff period";
+            foreach ($emails as $key => $notificationUserVal) {
+                $emailContent = BudgetCutOffNotificationService::getEmailContent($poData, $notificationUserVal[$key]['empName']);
+
+                $sendEmail = NotificationService::emailNotification($companyIDFromScenario, $subject, $notificationUserVal[$key]['empEmail'], $emailContent);
+
+                if (!$sendEmail["success"]) {
+                    Log::error($sendEmail["message"]);
+                }
+            }
         }
     }
 }
