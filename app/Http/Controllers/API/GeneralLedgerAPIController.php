45bd02a7748bf1f02b6a6680052b389e6ca4ed88
ThatSaravanan1112 <thatparanthan.saravanan@osos.om>
2022-09-12 21:45:34 +0530

    GCP-268 date added in backend removed

--- app/Http/Controllers/API/GeneralLedgerAPIController.php
+++ app/Http/Controllers/API/GeneralLedgerAPIController.php
@@ -948,7 +948,7 @@ class GeneralLedgerAPIController extends AppBaseController
         $input = $request->all();
 
         $toDate = (new   Carbon($request->toDate))->format('Y-m-d');
-        $fromDate = ((new Carbon($request->fromDate))->addDays(1)->format('Y-m-d'));
+        $fromDate = ((new Carbon($request->fromDate))->format('Y-m-d'));
         $type = $request->currency;
         $company = $request->company;
         $details = $this->generateGLReport($fromDate,$toDate,$type,$company);
