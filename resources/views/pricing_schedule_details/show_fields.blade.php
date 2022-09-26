d10450b5a8eaff5acb3801efd16cb81c61a0f1aa
nilaf246 <nilaf.ahamed@gears-int.com>
2022-09-26 08:53:21 +0530

    GCUP-740 Edit Option for the price bid formats

---
+++ resources/views/pricing_schedule_details/show_fields.blade.php
@@ -0,0 +1,84 @@
+<!-- Bid Format Id Field -->
+<div class="form-group">
+    {!! Form::label('bid_format_id', 'Bid Format Id:') !!}
+    <p>{{ $pricingScheduleDetail->bid_format_id }}</p>
+</div>
+
+<!-- Boq Applicable Field -->
+<div class="form-group">
+    {!! Form::label('boq_applicable', 'Boq Applicable:') !!}
+    <p>{{ $pricingScheduleDetail->boq_applicable }}</p>
+</div>
+
+<!-- Created At Field -->
+<div class="form-group">
+    {!! Form::label('created_at', 'Created At:') !!}
+    <p>{{ $pricingScheduleDetail->created_at }}</p>
+</div>
+
+<!-- Created By Field -->
+<div class="form-group">
+    {!! Form::label('created_by', 'Created By:') !!}
+    <p>{{ $pricingScheduleDetail->created_by }}</p>
+</div>
+
+<!-- Deleted At Field -->
+<div class="form-group">
+    {!! Form::label('deleted_at', 'Deleted At:') !!}
+    <p>{{ $pricingScheduleDetail->deleted_at }}</p>
+</div>
+
+<!-- Field Type Field -->
+<div class="form-group">
+    {!! Form::label('field_type', 'Field Type:') !!}
+    <p>{{ $pricingScheduleDetail->field_type }}</p>
+</div>
+
+<!-- Formula String Field -->
+<div class="form-group">
+    {!! Form::label('formula_string', 'Formula String:') !!}
+    <p>{{ $pricingScheduleDetail->formula_string }}</p>
+</div>
+
+<!-- Id Field -->
+<div class="form-group">
+    {!! Form::label('id', 'Id:') !!}
+    <p>{{ $pricingScheduleDetail->id }}</p>
+</div>
+
+<!-- Is Disabled Field -->
+<div class="form-group">
+    {!! Form::label('is_disabled', 'Is Disabled:') !!}
+    <p>{{ $pricingScheduleDetail->is_disabled }}</p>
+</div>
+
+<!-- Label Field -->
+<div class="form-group">
+    {!! Form::label('label', 'Label:') !!}
+    <p>{{ $pricingScheduleDetail->label }}</p>
+</div>
+
+<!-- Pricing Schedule Master Id Field -->
+<div class="form-group">
+    {!! Form::label('pricing_schedule_master_id', 'Pricing Schedule Master Id:') !!}
+    <p>{{ $pricingScheduleDetail->pricing_schedule_master_id }}</p>
+</div>
+
+<!-- Tender Id Field -->
+<div class="form-group">
+    {!! Form::label('tender_id', 'Tender Id:') !!}
+    <p>{{ $pricingScheduleDetail->tender_id }}</p>
+</div>
+
+<!-- Updated At Field -->
+<div class="form-group">
+    {!! Form::label('updated_at', 'Updated At:') !!}
+    <p>{{ $pricingScheduleDetail->updated_at }}</p>
+</div>
+
+<!-- Updated By Field -->
+<div class="form-group">
+    {!! Form::label('updated_by', 'Updated By:') !!}
+    <p>{{ $pricingScheduleDetail->updated_by }}</p>
+</div>
+
