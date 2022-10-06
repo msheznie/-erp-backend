47cdabe2d9434b0af28ee3d79a458776f673b54a
Zakeeul <shakeeul@osos.om>
2022-10-06 18:21:22 +0530

    GCP-353 fix the pv confirm issue more book

--- app/Http/Controllers/API/PaySupplierInvoiceMasterAPIController.php
+++ app/Http/Controllers/API/PaySupplierInvoiceMasterAPIController.php
@@ -1854,11 +1854,9 @@ class PaySupplierInvoiceMasterAPIController extends AppBaseController
 
                         if ($updatePayment) {
 
-                            $supplierPaidAmountSum = PaySupplierInvoiceDetail::selectRaw('erp_paysupplierinvoicedetail.apAutoID, erp_paysupplierinvoicedetail.supplierInvoiceAmount, Sum(erp_paysupplierinvoicedetail.supplierPaymentAmount) AS SumOfsupplierPaymentAmount')->when($paySupplierInvoiceMaster->invoiceType == 6, function($query) {
-                                    $query->whereHas('payment_master', function($query) {
-                                        $query->where('invoiceType',6);
-                                    });
-                                })->where('apAutoID', $val->apAutoID)->groupBy('erp_paysupplierinvoicedetail.apAutoID')->first();
+                            $supplierPaidAmountSum = PaySupplierInvoiceDetail::selectRaw('erp_paysupplierinvoicedetail.apAutoID, erp_paysupplierinvoicedetail.supplierInvoiceAmount, Sum(erp_paysupplierinvoicedetail.supplierPaymentAmount) AS SumOfsupplierPaymentAmount')->whereHas('payment_master', function($query) use ($paySupplierInvoiceMaster){
+                                $query->where('invoiceType',$paySupplierInvoiceMaster->invoiceType);
+                            })->where('apAutoID', $val->apAutoID)->groupBy('erp_paysupplierinvoicedetail.apAutoID')->first();
 
                             $matchedAmount = MatchDocumentMaster::selectRaw('erp_matchdocumentmaster.PayMasterAutoId, erp_matchdocumentmaster.documentID, Sum(erp_matchdocumentmaster.matchedAmount) AS SumOfmatchedAmount')->where('PayMasterAutoId', $val->bookingInvSystemCode)->where('documentSystemID', $val->addedDocumentSystemID)->groupBy('erp_matchdocumentmaster.PayMasterAutoId', 'erp_matchdocumentmaster.documentSystemID')->first();
 
