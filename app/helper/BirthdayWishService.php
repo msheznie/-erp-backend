686bde49d9064e4085a734e02df67c996b997cf7
Lakru <lakru.roshan@osos.om>
2023-01-09 15:06:48 +0530

    ASAAS-715 image not loading fixed

--- app/helper/BirthdayWishService.php
+++ app/helper/BirthdayWishService.php
@@ -13,8 +13,6 @@ class BirthdayWishService
     public $companyId;
     public $companyCode;
     public $image;
-    public $image2;
-    public $image3;
     public $switchDb;
 
     public function __construct($companyData, $db='')
@@ -23,9 +21,6 @@ class BirthdayWishService
         $this->companyCode = $companyData['code'];
         $this->switchDb = $db;
         $this->image = asset("image/Birthday-ASAAS-01.jpg");
-        $this->image2 = "https://gearsentattachments-qa.s3.us-west-1.amazonaws.com/BG/logos/BG_logo.png?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAX7MLAY73AFHLFUGD%2F20230109%2Fus-west-1%2Fs3%2Faws4_request&X-Amz-Date=20230109T072408Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=5663f03c89ffe4be54deef4473d334b1d1cad74fe33e2c339959548e7f974c36";
-        $this->image3 = asset("logos/BIT_logo.png");
-
     }
 
     function execute()
@@ -79,9 +74,7 @@ class BirthdayWishService
 
             $emailData['empEmail'] = $employee->EEmail;
             $emailData['companySystemID'] = $employee->Erp_companyID;
-            $temp = '<img src= '.$this->image.' />
-                     <br><img src= '.$this->image2.' />
-                     <br><img src= '.$this->image3.' /> ';
+            $temp = '<img src= '.$this->image.' />';
             $emailData['alertMessage'] = "Happy Birthday $employee->Ename2.";
             $emailData['emailAlertMessage'] = $temp;
             $sendEmail = \Email::sendEmailErp($emailData);
