18145f2d3606e866d078b4515d0f70eea2db26bb
Lakru <lakru.roshan@osos.om>
2023-01-31 11:21:15 +0530

    ASAAS-718 change to time old 2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('command:birthday_wish_schedule')
             ->timezone('Asia/Muscat')
-            ->dailyAt('08:30')
+            ->dailyAt('02:00')
             ->withoutOverlapping();
     }
 
