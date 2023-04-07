137be7a0e0914d4bba799bb48da8799df4a18480
nasik <nasik@gears-int.com>
2023-04-07 16:17:35 +0530

    GHR-367 notification mail debug

---
+++ resources/views/print/travel_request_dummy.blade.php
@@ -0,0 +1,186 @@
+<html>
+
+<head>
+    <title>Travel Request</title>
+    <style>
+        @page {
+            margin-left: 30px;
+            margin-right: 30px;
+            margin-top: 30px;
+        }
+
+        .footer {
+            position: absolute;
+        }
+
+        .footer {
+            bottom: 0;
+            height: 100px;
+        }
+
+        .footer {
+            width: 100%;
+            text-align: center;
+            position: fixed;
+            font-size: 10px;
+            padding-top: -20px;
+        }
+
+        body {
+            font-size: 12px;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        }
+
+        h3 {
+            font-size: 24.5px;
+        }
+
+        h6 {
+            font-size: 14px;
+        }
+
+        h6,
+        h3 {
+            margin-top: 0px;
+            margin-bottom: 0px;
+            font-family: inherit;
+            font-weight: bold;
+            line-height: 1.2;
+            color: inherit;
+        }
+
+        table>tbody>tr>td {
+            font-size: 11.5px;
+        }
+
+        .theme-tr-head {
+            background-color: rgb(215, 215, 215) !important;
+        }
+
+        .text-left {
+            text-align: left;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .font-weight-bold {
+            font-weight: 700 !important;
+        }
+
+        .table thead th {
+            border-bottom: none !important;
+        }
+
+        .white-space-pre-line {
+            white-space: pre-line;
+            white-space: pre;
+            word-wrap: normal;
+        }
+
+        .text-muted {
+            color: #dedede !important;
+        }
+
+        .font-weight-bold {
+            font-weight: 700 !important;
+        }
+
+        .table thead th {
+            vertical-align: bottom;
+            border-bottom: 2px solid #c2cfd6;
+        }
+
+        table.table-bordered {
+            border: 1px solid #000;
+        }
+
+        .table th,
+        .table td {
+            padding: 6.4px !important;
+        }
+
+        table.table-bordered {
+            border-collapse: collapse;
+        }
+
+        table.table-bordered,
+        .table-bordered th,
+        .table-bordered td {
+            border: 1px solid #e2e3e5;
+        }
+
+        table>thead>tr>th {
+            font-size: 11.5px;
+        }
+
+        hr {
+            margin-top: 16px;
+            margin-bottom: 16px;
+            border: 0;
+            border-top: 1px solid
+        }
+
+        hr {
+            -webkit-box-sizing: content-box;
+            box-sizing: content-box;
+            height: 0;
+            overflow: visible;
+        }
+
+        .header {
+            top: 0px;
+        }
+
+        .pagenum:before {
+            content: counter(page);
+        }
+
+        #watermark {
+            position: fixed;
+            bottom: 0px;
+            right: 0px;
+            width: 200px;
+            height: 200px;
+            opacity: .1;
+        }
+
+        .content {
+            margin-bottom: 100px;
+        }
+
+        .border-top-remov {
+            border-top: 1px solid #ffffff00 !important;
+            border-left: 1px solid #ffffff00 !important;
+            background-color: #ffffff !important;
+            border-right: 0;
+        }
+
+        .border-bottom-remov {
+            border-bottom: 1px solid #ffffffff !important;
+            background-color: #ffffff !important;
+            border-right: 1px solid #ffffffff !important;
+        }
+
+        .container {
+            display: block;
+            max-width: 230px;
+            max-height: 95px;
+            width: auto;
+            height: auto;
+        }
+
+        .table_height {
+            max-height: 60px !important;
+        }
+    </style>
+</head>
+
+<body>
+     test
+</body>
