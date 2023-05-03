a47a3bebd362fbd4c8383239c1622d87b9d69635
yasasaJ <130026804+yasasaJ@users.noreply.github.com>
2023-05-03 15:02:47 +0530

    Revert "GCP-1367 added a join query to check debit note type"
    This reverts commit 59f8d6ea2dd879dd6c420030ab5922c64781717a.

--- app/Http/Controllers/API/MatchDocumentMasterAPIController.php
+++ app/Http/Controllers/API/MatchDocumentMasterAPIController.php
@@ -2448,8 +2448,7 @@ class MatchDocumentMasterAPIController extends AppBaseController
                 FROM
                     erp_paysupplierinvoicedetail
                 INNER JOIN erp_paysupplierinvoicemaster ON erp_paysupplierinvoicemaster.PayMasterAutoId = erp_paysupplierinvoicedetail.PayMasterAutoId
-                JOIN erp_debitnote ON erp_paysupplierinvoicedetail.PayMasterAutoId = erp_debitnote.debitNoteAutoID
-                WHERE erp_paysupplierinvoicemaster.invoiceType != 6 AND erp_paysupplierinvoicemaster.invoiceType != 7 AND erp_debitnote.type = 2
+                WHERE erp_paysupplierinvoicemaster.invoiceType != 6 AND erp_paysupplierinvoicemaster.invoiceType != 7
                 GROUP BY
                     erp_paysupplierinvoicedetail.apAutoID
                     ) sid ON sid.apAutoID = erp_accountspayableledger.apAutoID
