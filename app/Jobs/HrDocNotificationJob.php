850dc66603bd2bee821627672571cc10fd46f139
nuskagears <nuska@gears-int.com>
2023-05-18 16:07:35 +0400

    GHR-452 job service

---
+++ app/Jobs/HrDocNotificationJob.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\helper\CommonJobService;
+use App\Services\hrms\travelRequest\HrDocNotificationService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class HrDocNotificationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    public $tenantId;
+    public $companyId; 
+    public $id; 
+    public $emlpoyees; 
+    public $visibility;
+
+    public function __construct($tenantId, $companyId, $id, $visibility, $emlpoyees)
+    {
+        if(env('IS_MULTI_TENANCY',false)){
+            self::onConnection('database_main');
+        }else{
+            self::onConnection('database');
+        }
+        
+        $this->tenantId = $tenantId;
+        $this->companyId = $companyId;
+        $this->id = $id; 
+        $this->visibility = $visibility;
+        $this->emlpoyees = $emlpoyees; 
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        
+        $db_name = CommonJobService::get_tenant_db($this->tenantId);
+        
+        if (empty($db_name)) {
+            $message = "db details not found. \t on file: " . __CLASS__ . " \tline no :" . __LINE__;
+            $data = [
+                'company_id' => $this->companyId,
+                'module' => 'HRMS',
+                'description' => 'HR Document Notification Scenario Error',
+                'scenario_id' => 0,
+                'processed_for' => Carbon::now()->format('Y-m-d H:i:s'),
+                'logged_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'log_type' => 'error',
+                'log_data' => json_encode($message)
+            ];
+            DB::table('job_logs')->insert($data);
+        } else {
+           
+            CommonJobService::db_switch($db_name);
+            $obj = new HrDocNotificationService($this->companyId, $this->tenantId, $this->id ,$this->visibility ,$this->emlpoyees);
+            $obj->execute();
+        }
+    }
+}
---
+++ app/Services/hrms/hrDocument/HrDocNotificationService.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Services\hrms\travelRequest;
+
+use Collator;
+use Exception;
+use Carbon\Carbon;
+use App\helper\CommonJobService;
+use App\Models\Company;
+use App\Models\NotificationCompanyScenario;
+use App\Models\SrpEmployeeDetails;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class HrDocNotificationService
+{
+    private $companyId;
+    private $tenantId;
+    private $id;
+    private $date;
+    private $visibility;
+    private $employees;
+    private $notifyList;
+    private $documentCode;
+
+
+    public function __construct($companyId, $tenantId, $id, $visibility, $employees)
+    {
+        $this->companyId = $companyId;
+        $this->tenantId = $tenantId;
+        $this->id = $id;
+        $this->date = Carbon::now()->format('Y-m-d H:i:s');
+        $this->visibility = $visibility;
+        $this->employees = $employees;
+        $this->notifyList = [];
+        // $this->documentCode = $this->master['documentDescription'];
+        $this->documentCode = $id;
+    }
+
+    function execute()
+    { 
+        
+        $this->insertToLogTb(['Document Code'=> $this->id ,'Message'=> 'one'],'error');  
+        
+        if ( $this->visibility == 2 && empty($this->employees)) {
+            $this->insertToLogTb(['Document Code'=> $this->id ,'Message'=> 'Employees does not exists'],'error');  
+            return false;
+        }
+
+        $this->validateNotifyEmpDataExists();
+        $this->insertToLogTb(['Document Code'=> $this->id ,'Message'=> 'Employees data does not exists'.$this->notifyList],'error');  
+
+        if (empty($this->notifyList)) {
+            $this->insertToLogTb(['Document Code'=> $this->id ,'Message'=> 'Employees data does not exists'],'error');  
+            return false;
+        }
+        $this->sendEmail();
+        $this->insertToLogTb([ 'Document Code'=> $this->documentCode ,'Message'=> 'Execution successfully completed']);
+    }
+
+    public function sendEmail()
+    { 
+        $this->insertToLogTb(['Document Code'=> $this->id ,'Message'=> 'two'],'error');  
+
+        $this->insertToLogTb([ 'Document Code'=> $this->documentCode ,'Message'=> 'Email Function Triggered']);
+        $msg = '';
+        $logType = 'info';
+        // $this->generateTravelRequestPdf(); 
+        // $dataEmail['attachmentFileName'] = $this->pdfName; 
+    
+
+        foreach ($this->notifyList as $val) {
+            $dataEmail['empEmail'] = $val['EEmail'];
+            $dataEmail['companySystemID'] = $this->companyId;
+            $temp = '<p>Dear ' . $val['Ename2'] . ', <br /></p>';
+            $temp .=  '<p> HR has uploaded a new document, please login to download it.</p>';
+            $dataEmail['emailAlertMessage'] = $temp;
+            $dataEmail['alertMessage'] = 'New HR Document';
+            $sendEmail = \Email::sendEmailErp($dataEmail);
+            if (!$sendEmail["success"]) {
+                $msg = "HR Document notification not sent for {$val['EIdNo']} | {$val['Ename2']} "; 
+                $logType = 'error';
+            }else { 
+                $msg = "HR Document notification sent for {$val['EIdNo']} | {$val['Ename2']} ";
+            }
+            $this->insertToLogTb(['Document Code'=> $this->documentCode ,'Message'=> $msg],$logType); 
+        }
+    }
+
+    
+    public function validateNotifyEmpDataExists()
+    {
+        $getNotifyEmployees = $this->getEmpDetails();
+        $this->notifyList = (!empty($getNotifyEmployees) ? $getNotifyEmployees : []);
+    }
+
+    public function getEmpDetails()
+    { 
+        $notifyEmpData = SrpEmployeeDetails::select('EIdNo', 'Ename2', 'EEmail')
+            ->whereIn('EIdNo', $this->employees)
+            ->get()
+            ->toArray();
+        
+        return $notifyEmpData;
+    }
+
+    public function getCompanyData(){ 
+        return Company::where('companySystemID', $this->companyId)
+        ->first();
+    }
+
+    public function insertToLogTb($logData, $logType = 'info')
+    {
+        $logData = json_encode($logData);
+        $data = [
+            'company_id' => $this->companyId,
+            'module' => 'HRMS',
+            'description' => 'HR Document Notification Scenario',
+            'scenario_id' => 20,
+            'processed_for' => $this->date,
+            'logged_at' => $this->date,
+            'log_type' => $logType,
+            'log_data' => $logData,
+        ];
+        DB::table('job_logs')->insert($data);
+    }
+}
