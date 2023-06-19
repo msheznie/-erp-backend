75c662d7926fe4c7fcee0646476a8e37b108775e
Aflal <aflal@gears-int.com>
2023-06-19 17:27:18 +0530

    GSUP-1188 Fixed a issue

--- app/Http/Controllers/API/BidSubmissionMasterAPIController.php
+++ app/Http/Controllers/API/BidSubmissionMasterAPIController.php
@@ -1169,7 +1169,7 @@ class BidSubmissionMasterAPIController extends AppBaseController
                 ->havingRaw('weightage >= passing_weightage')
                 ->groupBy('srm_bid_submission_master.id')
                 ->where('srm_bid_submission_master.status', 1)
-                ->where('srm_bid_submission_master.doc_verifiy_status','!=',2)
+                ->where('srm_bid_submission_master.doc_verifiy_status','=',1)
                 ->where('srm_bid_submission_master.bidSubmittedYN', 1)
                 ->where('srm_bid_submission_master.tender_id', $tenderId)
                 ->orderBy('srm_bid_submission_master.id', 'desc');
@@ -1186,7 +1186,7 @@ class BidSubmissionMasterAPIController extends AppBaseController
                 ->groupBy('srm_bid_submission_master.id')
                 ->where('srm_bid_submission_master.status', 1)
                 ->where('srm_bid_submission_master.bidSubmittedYN', 1)
-                ->where('srm_bid_submission_master.doc_verifiy_status','!=',2)
+                ->where('srm_bid_submission_master.doc_verifiy_status','=',1)
                 ->where('srm_bid_submission_master.tender_id', $tenderId)
                 ->orderBy('srm_bid_submission_master.id', 'desc');
         }
