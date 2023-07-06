175f5121232235ea34bd49c6d3a3769e2c92f092
nilaf246 <nilaf.ahamed@gears-int.com>
2023-07-06 16:22:49 +0530

    GCP-1520 Amount in detail level can be reduced after allocating to assets and employees in material issue and payment voucher

--- app/Http/Controllers/API/ItemIssueDetailsAPIController.php
+++ app/Http/Controllers/API/ItemIssueDetailsAPIController.php
@@ -51,6 +51,8 @@ use App\Repositories\UserRepository;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\ExpenseAssetAllocation;
+use App\Models\ExpenseEmployeeAllocation;
+
 /**
  * Class ItemIssueDetailsController
  * @package App\Http\Controllers\API
@@ -989,8 +991,17 @@ class ItemIssueDetailsAPIController extends AppBaseController
                 if ($allocatedSum > $input['qtyIssued']) {
                     return $this->sendError("Allocated quantity cannot be greater than the detail quantity.");
                 }
-               
 
+                $allocatedQtySum = ExpenseEmployeeAllocation::where('documentDetailID', $input['itemIssueDetailID'])
+                ->where('documentSystemID', $itemIssue->documentSystemID)
+                ->where('documentSystemCode', $input['itemIssueAutoID'])
+                ->sum('assignedQty');
+
+                
+               
+                if ($allocatedQtySum > $input['qtyIssued']) {
+                    return $this->sendError("Allocated quantity cannot be greater than the detail quantity.");
+                }
             }
 
         $itemIssueDetails = $this->itemIssueDetailsRepository->update($input, $id);
