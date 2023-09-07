2275ef912468921f060b72345634d9f59fb9ffaa
Zakeeul <60167775+zakeeul-gears@users.noreply.github.com>
2023-09-07 14:59:50 +0530

    GCP-1882 update param of mail

--- app/helper/email.php
+++ app/helper/email.php
@@ -542,7 +542,7 @@ class email
                     if (isset($data['empEmail']) && $data['empEmail']) {
                         $data['empEmail'] = self::emailAddressFormat($data['empEmail']);
                         if ($data['empEmail']) {
-                            Mail::to($data['empEmail'])->send(new EmailForQueuing($data['alertMessage'], $data['emailAlertMessage'], $data['attachmentFileName'],null,$color,$text));
+                            Mail::to($data['empEmail'])->send(new EmailForQueuing($data['alertMessage'], $data['emailAlertMessage'], $data['attachmentFileName'],[],$color,$text));
                         }
                     }
                     Log::info('email sent success fully to :' . $data['empEmail']);
