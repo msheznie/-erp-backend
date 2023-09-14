9a62fcd3341c1dfa8a3265259f751b29125776a3
Jayan Anuranga <jayan.anuranga@gears-int.com>
2023-09-14 10:06:28 +0530

    GSUP-1198 - allow tender added PR for PRX

--- app/Http/Controllers/API/TenderMasterAPIController.php
+++ app/Http/Controllers/API/TenderMasterAPIController.php
@@ -743,17 +743,22 @@ WHERE
             }
         }
         $data['documentTypes'] = $docTypeArr;
-
+        
         // Get Purchase Request Data
         $purchaseRequest = PurchaseRequest::select('purchaseRequestID as id', 'purchaseRequestCode')
             ->with(['tender_purchase_request','details.podetail'])
             ->where('approved', '-1')
-            ->where('companySystemID', $companySystemID)
-            ->whereDoesntHave('tender_purchase_request', function ($query) use ($tenderMasterId) {
+            ->where('companySystemID', $companySystemID);
+
+        if($data['master']->document_type == 0){
+            $purchaseRequest = $purchaseRequest->whereDoesntHave('tender_purchase_request', function ($query) use ($tenderMasterId) {
                 $query->where('tender_id','!=' ,$tenderMasterId);
-            })
-            ->whereDoesntHave('details.podetail')
+            });
+        }
+
+           $purchaseRequest =  $purchaseRequest->whereDoesntHave('details.podetail')
             ->get();
+
         $data['purchaseRequest'] = $purchaseRequest;
 
         // Get Tender Purchase Request Data
