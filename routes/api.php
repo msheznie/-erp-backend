6b6a2b03c9feb1b4f2416a3a165f42e4b8dd5530
yasasaJ <130026804+yasasaJ@users.noreply.github.com>
2023-10-26 18:41:37 +0530

    GCP-2012 backend query for all bills and export excel

--- routes/api.php
+++ routes/api.php
@@ -698,7 +698,8 @@ Route::group(['middleware' => ['tenant','locale']], function () {
             Route::post('getPosMissMatchData', 'ShiftDetailsAPIController@getPosMissMatchData');
             Route::post('updatePosMismatch', 'ShiftDetailsAPIController@updatePosMismatch');
             Route::post('getGlMatchEntries', 'ShiftDetailsAPIController@getGlMatchEntries');
-            
+            Route::post('exportInsufficientItems', 'ShiftDetailsAPIController@exportInsufficientItems');
+
             Route::resource('currency_denominations', 'CurrencyDenominationAPIController');
             Route::resource('shift_details', 'ShiftDetailsAPIController');
             Route::get('getPosCustomerSearch', 'CustomerMasterAPIController@getPosCustomerSearch');
@@ -1153,6 +1154,7 @@ Route::group(['middleware' => ['tenant','locale']], function () {
             Route::post('getAllInvoicesRPos', 'POS\PosAPIController@getAllInvoicesRPos');
             Route::post('getRPOSInvoiceData', 'POS\PosAPIController@getRPOSInvoiceData');
             Route::post('getAllShiftsGPOS', 'POS\PosAPIController@getAllShiftsGPOS');
+            Route::post('getAllBills', 'POS\PosAPIController@getAllBills');
 
             Route::post('generateGeneralLedgerReportPDF', 'FinancialReportAPIController@pdfExportReport');
             Route::post('generateFinancialTrialBalanceReportPDF', 'FinancialReportAPIController@pdfExportReport');
