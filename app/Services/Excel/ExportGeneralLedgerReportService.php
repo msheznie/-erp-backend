ebced64531b371658b5ba3ce08ca3a7c5b4aafb5
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2024-01-01 16:00:51 +0530

    chore(common): commented codes removed [GCP-2544] (#5376)
    * feat(reports): export excel report date and currency format changes [GCP-2544]
    
    * feat(reports): excel currency changed to number format and comma added as seprator [GCP-2544]
    
    * chore(common): commented codes removed [GCP-2544]
    
    ---------
    
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- app/Services/Excel/ExportGeneralLedgerReportService.php
+++ app/Services/Excel/ExportGeneralLedgerReportService.php
@@ -5,21 +5,4 @@ namespace App\Services\Excel;
 class ExportGeneralLedgerReportService extends ExportReportToExcelService
 {
 
-//    public function setDetails(): ExportReportToExcelService
-//    {
-//
-//        $detail_array = array(  'type' => 1,
-//            'from_date'=>$this->fromDate,
-//            'to_date'=>$this->toDate,
-//            'company_name'=>$this->companyName,
-//            'company_code'=>$this->companyCode,
-//            'cur'=>$this->currency,
-//            'title'=>$this->title,
-//            'excelFormat' => $this->excelFormat,
-//            'dataType' => $this->dataType
-//        );
-//
-//        $this->details = $detail_array;
-//        return $this;
-//    }
 }
