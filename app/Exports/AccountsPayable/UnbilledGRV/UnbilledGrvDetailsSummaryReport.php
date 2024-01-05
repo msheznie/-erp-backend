12d2a7212fcddc5f958a716bbf2d27705cc8c1c9
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2024-01-05 14:43:56 +0530

    chore(accounts payable): unbilled grv summary report excel not comes in number fixed [GCP-2672] (#5415)
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- app/Exports/AccountsPayable/UnbilledGRV/UnbilledGrvDetailsSummaryReport.php
+++ app/Exports/AccountsPayable/UnbilledGRV/UnbilledGrvDetailsSummaryReport.php
@@ -93,7 +93,12 @@ class UnbilledGrvDetailsSummaryReport
     public function getCloumnFormat()
     {
         return [
-            'D' => \PHPExcel_Style_NumberFormat::FORMAT_GENERAL
+            'D' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'E' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'F' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'G' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'H' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'I' => \PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1
         ];
     }
     public function getHeader() :Array {
