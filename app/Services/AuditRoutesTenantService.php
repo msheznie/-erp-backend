27858d6ce9110683be0fbcd640d377bee9791127
yasasaJ <130026804+yasasaJ@users.noreply.github.com>
2024-01-26 14:14:35 +0530

    fix(system admin): item finance category edit 'Is Active' toggle not recording in audit log fixed [GCP-2726] (#5510)

--- app/Services/AuditRoutesTenantService.php
+++ app/Services/AuditRoutesTenantService.php
@@ -8,7 +8,7 @@ class AuditRoutesTenantService
         $lokiTenantRoutes = [
             'api/v1/auditLogs',
             'api/v1/financeItemCategorySubsExpiryUpdate',
-            'api/v1/finance_item_category_subs',
+            'api/v1/finance_item_category_subs/{finance_item_category_sub}',
             'api/v1/finance_item_category_subs_update',
             'api/v1/financeItemCategorySubsAttributesUpdate'
         ];
