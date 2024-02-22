1692f2ccab62f9ec4ee5a8be49d3d11b50f3686a
yasasaJ <130026804+yasasaJ@users.noreply.github.com>
2024-02-22 19:34:08 +0530

    chore(asset management): excel changes in asset tracking report [GCP-2578] (#5709)

--- resources/views/export_report/asset_tracking.blade.php
+++ resources/views/export_report/asset_tracking.blade.php
@@ -37,11 +37,38 @@
                 <td class="text-left">{{ $data->documentCode }}</td>
                 <td class="text-left">{{ \Carbon\Carbon::parse($data->documentDate)->format('d/m/Y') }}</td>
                 <td class="text-left">{{ $data->transferType }}</td>
+                @if($data->type != 3)
                 <td class="text-left">{{ $data->fromName }}</td>
+                @endif
+                @if($data->type == 3)
+                <td class="text-left">{{ $data->fromEmpName }}</td>
+                @endif
+
+                @if($data->type != 3 && $data->type != 1 && $data->type != 4)
                 <td class="text-left">{{ $data->toName }}</td>
-                <td class="text-left">{{ $data->locationName }}</td>
-                <td class="text-left">{{ $data->depName }}</td>
+                @endif
+                @if($data->type == 3)
+                <td class="text-left">{{ $data->toEmpName }}</td>
+                @endif
+                @if($data->type == 1)
                 <td class="text-left">{{ $data->reqName }}</td>
+                @endif
+                @if($data->type == 4)
+                <td class="text-left">{{ $data->transferDepName }}</td>
+                @endif
+                <td class="text-left">{{ $data->locationName }}</td>
+                @if($data->type == 4)
+                    <td class="text-left">{{ $data->transferDepName }}</td>
+                @endif
+                @if($data->type != 4)
+                <td class="text-left">{{ $data->depName }}</td>
+                @endif
+                @if($data->type == 3)
+                <td class="text-left">{{ $data->toEmpName }}</td>
+                @endif
+                @if($data->type != 3)
+                <td class="text-left">{{ $data->reqName }}</td>
+                @endif
             </tr>
     @endforeach
     </tbody>
