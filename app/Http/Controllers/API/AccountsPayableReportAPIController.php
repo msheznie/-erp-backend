bfa867aa4bf79048701956e8d93c0aefc7b4c1de
yasasaJ <130026804+yasasaJ@users.noreply.github.com>
2024-02-26 08:55:12 +0530

    chore(accounts payable): added matchDocID conditon [GCP-2854] (#5714)

--- app/Http/Controllers/API/AccountsPayableReportAPIController.php
+++ app/Http/Controllers/API/AccountsPayableReportAPIController.php
@@ -2043,7 +2043,8 @@ class AccountsPayableReportAPIController extends AppBaseController
                                 erp_paysupplierinvoicedetail
                                 INNER JOIN erp_paysupplierinvoicemaster ON erp_paysupplierinvoicedetail.PayMasterAutoId = erp_paysupplierinvoicemaster.PayMasterAutoId
                             WHERE
-                                erp_paysupplierinvoicedetail.isRetention = 0
+                                erp_paysupplierinvoicedetail.matchingDocID = 0
+                                AND erp_paysupplierinvoicedetail.isRetention = 0
                                 AND erp_paysupplierinvoicemaster.approved = -1
                                 AND erp_paysupplierinvoicemaster.companySystemID IN (' . join(',', $companyID) . ')
                                 AND erp_paysupplierinvoicedetail.supplierCodeSystem IN (' . join(',', $supplierSystemID) . ')
