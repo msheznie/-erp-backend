579059dbd551ef9e3c8df1ba991c4fb387cd3752
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2024-03-04 11:46:28 +0530

    feat(system admin): not showing not approved CA in finance sub category creations [GCP-3067] (#5767)

--- app/Http/Controllers/API/FinanceItemCategoryMasterAPIController.php
+++ app/Http/Controllers/API/FinanceItemCategoryMasterAPIController.php
@@ -183,7 +183,7 @@ class FinanceItemCategoryMasterAPIController extends AppBaseController
         $chartOfAccount = ChartOfAccount::all();
 
         /** expense chart of accounts */
-        $expenseChartOfAccount = ChartOfAccount::whereIn('controlAccountsSystemID',[2,3,4])->where('isActive',1)->get();
+        $expenseChartOfAccount = ChartOfAccount::whereIn('controlAccountsSystemID',[2,3,4])->where('isActive',1)->where('isApproved',1)->get();
 
         /** Yes and No Selection */
         $yesNoSelection = YesNoSelection::all();
