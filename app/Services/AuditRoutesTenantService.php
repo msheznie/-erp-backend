fab568a13c90dfdb214fecf449477864a0496a19
Arshadgears <86824223+Arshadgears@users.noreply.github.com>
2024-04-26 11:18:18 +0530

    chore(system admin): Logs not showing in Asset finance category [GCP-3444] (#6031)

--- app/Services/AuditRoutesTenantService.php
+++ app/Services/AuditRoutesTenantService.php
@@ -19,7 +19,7 @@ class AuditRoutesTenantService
             'api/v1/supplier/masters/update',
             'api/v1/chart_of_account',
             'api/v1/updateItemMaster',
-            'api/v1/asset_finance_categories'
+            'api/v1/asset_finance_categories/{asset_finance_category}'
         ];
 
         return $lokiTenantRoutes;
