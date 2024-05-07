6e83e1de0bf6c43094c6f144f39ea239799d107e
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2024-05-07 10:05:58 +0530

    chore(inventory): modification on delegation auto active function [GCP-3508] (#6090)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('itemWACAmountPost')->daily()->withoutOverlapping();
         $schedule->command('command:recurringVoucher')->daily()->withoutOverlapping();
         $schedule->command('command:reversePoAccrual')->daily()->withoutOverlapping();
-        $schedule->command('command:delegationActive')->everyMinute()->withoutOverlapping();
+        $schedule->command('command:delegationActive')->daily()->withoutOverlapping();
 
         $schedule->command('pull-attendance')
         ->timezone('Asia/Muscat')
