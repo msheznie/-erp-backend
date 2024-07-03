bfe479dfb44584a3aa1d17c1584085f443d42dc3
nilaf246 <nilaf.ahamed@gears-int.com>
2024-07-03 17:12:58 +0530

    feat(asset management): barcode company name update [GCP-3956Temp]

--- app/Http/Controllers/API/BarcodeConfigurationAPIController.php
+++ app/Http/Controllers/API/BarcodeConfigurationAPIController.php
@@ -399,7 +399,9 @@ class BarcodeConfigurationAPIController extends AppBaseController
 
         $configuration = BarcodeConfiguration::where('companySystemID',$subCompanies[0])->first();
         $company = Company::find($subCompanies[0]);
+
         $companyID = $company?$company->CompanyName:null;
+
         $logo = $company->logo_url && $company->logo_url != null?$company->logo_url:null;
         $companyLogo = $company->companyLogo;
         $companyArabicName = $company->CompanyNameLocalized;
