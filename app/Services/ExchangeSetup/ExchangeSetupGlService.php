1d278d50b4fcdb2b2f58f2fb37c72f2e432a2572
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2024-07-30 11:17:06 +0530

    chore(accounts payable): Supplier payment GL entries issue fixed [GCP-4123] (#6503)
    * chore(accounts payable): supplier payment GL entries issue fixed [GCP-4123]
    
    * chore(accounts payable): supplier advnaced payment gain or loss removed based on setup [GCP-4123]
    
    ---------
    
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- app/Services/ExchangeSetup/ExchangeSetupGlService.php
+++ app/Services/ExchangeSetup/ExchangeSetupGlService.php
@@ -30,96 +30,16 @@ class ExchangeSetupGlService
         switch ($masterData->invoiceType)
         {
             case 2:
+            case 5:
+            case 7:
             case 6:
-                $exchangeRatesLinkedDocuments = Arr::only($linkedDocument->toArray(), $linkedDocumentIndexArray);
-
-                if(isset($finalData[2]))
-                {
-
-                    $values = Arr::only($finalData[2], $changeIndexArray);
-                    $bankValues =  Arr::only($finalData[1], $changeIndexArray);
-
-                    if($values['documentLocalAmount'] < 0)
-                    {
-                        $bankValues['documentLocalAmount'] += $values['documentLocalAmount'];
-                    }else{
-                        $bankValues['documentLocalAmount'] -= $values['documentLocalAmount'];
-                    }
-
-                    if($values['documentRptAmount'] < 0)
-                    {
-                        $bankValues['documentRptAmount'] += $values['documentRptAmount'];
-                    }else {
-                        $bankValues['documentRptAmount'] -= $values['documentRptAmount'];
-                    }
-
-
-                    foreach ($bankValues as $key => $value) {
-                        if (array_key_exists($key, $finalData[1])) {
-                            $finalData[1][$key] = $value;
-                        }
-                    }
-                }
-
                 if(isset($finalData) && isset($finalData[2]))
                     unset($finalData[2]);
 
-                $data = array();
-
-                foreach ($finalData as $dt)
-                {
-                    $dt['documentLocalCurrencyER'] = $exchangeRatesLinkedDocuments['localCurrencyER'];
-                    $dt['documentRptCurrencyER'] = $exchangeRatesLinkedDocuments['reportingCurrencyER'];
-                    array_push($data,$dt);
-                }
-                $finalData = $data;
                 break;
             case 3:
-                $conversion = CurrencyConversion::where('masterCurrencyID', $masterData->supplierTransCurrencyID)->where('subCurrencyID', $masterData->localCurrencyID)->first();
-                $conversionRpt = CurrencyConversion::where('masterCurrencyID', $masterData->supplierTransCurrencyID)->where('subCurrencyID', $masterData->companyRptCurrencyID)->first();
-                $bankCurrencyConversion = CurrencyConversion::where('masterCurrencyID', $masterData->supplierTransCurrencyID)->where('subCurrencyID', $masterData->BPVbankCurrency)->first();
-                $exchangeRatesLinkedDocuments = [];
-                $exchangeRatesLinkedDocuments['documentLocalCurrencyER'] = $conversion['conversion'];
-                $exchangeRatesLinkedDocuments['documentRptCurrencyER'] = $conversionRpt['conversion'];
-                $exchangeRatesLinkedDocuments['BPVbankCurrencyER'] = $bankCurrencyConversion['conversion'];
-
-                $values = Arr::only($finalData[3], $changeIndexArray);
-                $bankValues =  Arr::only($finalData[1], $changeIndexArray);
-
-                if($values['documentLocalAmount'] < 0)
-                {
-                    $bankValues['documentLocalAmount'] += $values['documentLocalAmount'];
-                }else{
-                    $bankValues['documentLocalAmount'] -= $values['documentLocalAmount'];
-                }
-
-                if($values['documentRptAmount'] < 0)
-                {
-                    $bankValues['documentRptAmount'] += $values['documentRptAmount'];
-                }else {
-                    $bankValues['documentRptAmount'] -= $values['documentRptAmount'];
-                }
-
-
-                foreach ($bankValues as $key => $value) {
-                    if (array_key_exists($key, $finalData[1])) {
-                        $finalData[1][$key] = $value;
-                    }
-                }
-
                 if(isset($finalData) && isset($finalData[3]))
                     unset($finalData[3]);
-
-                $data = [];
-
-                foreach ($finalData as $dt)
-                {
-                    $dt['documentLocalCurrencyER'] = $exchangeRatesLinkedDocuments['documentLocalCurrencyER'];
-                    $dt['documentRptCurrencyER'] = $exchangeRatesLinkedDocuments['documentRptCurrencyER'];
-                    $dt['BPVbankCurrencyER'] = $exchangeRatesLinkedDocuments['BPVbankCurrencyER'];
-                    array_push($data,$dt);
-                }
-                $finalData = $data;
                 break;
         }
 
