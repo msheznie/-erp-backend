fac6708561402bdd3be28378018802a237b86305
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2024-10-14 18:15:31 +0530

    fix(budget): enhance budget upload export to include multi-level details in excel output [GCP-4680] (#6920)
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- resources/views/export_report/budget_details.blade.php
+++ resources/views/export_report/budget_details.blade.php
@@ -51,158 +51,204 @@
     </tr>
     </thead>
     @foreach($budgetDetails as $item)
-            <tr>
-                 <?php $mainNo = $loop->index + 1 ?>
-                <td>{{ $loop->index + 1 }}</td>
-                <td>{{ $item->description }}</td>
+        <tr>
+                <?php $mainNo = $loop->index + 1 ?>
+            <td>{{ $loop->index + 1 }}</td>
+            <td>{{ $item->description }}</td>
 
 
 
-                     <?php $type = $item->itemType ?>
-                     <?php $subCategoryTot = $item->subcategorytot ?>
-                     <?php $totNetAmounts = 0 ?>
-                     <?php $totYearNetAmounts = 0 ?>
+                <?php $type = $item->itemType ?>
+                <?php $subCategoryTot = $item->subcategorytot ?>
+                <?php $totNetAmounts = 0 ?>
+                <?php $totYearNetAmounts = 0 ?>
 
-                     <?php $subTotId = array(); ?>
-
-
-
-                 @if($type == 3 && $subCategoryTot != "undefined")
-                                @foreach($subCategoryTot as $totSubCat)
-
-                                    <?php
-                                      $subId = $totSubCat->subcategory->detID;
-                             array_push($subTotId,$subId);
-                                    ?>
-
-                                @endforeach
-                     @endif
+                <?php $subTotId = array(); ?>
 
 
 
-                     @if($type == 3)
+            @if($type == 3 && $subCategoryTot != "undefined")
+                @foreach($subCategoryTot as $totSubCat)
 
-                                    <?php $totBudget = 0 ?>
-                                    <?php $totYearBudget = 0 ?>
-                                    @foreach ($months as $month)
-                                        <?php $totBudget = 0 ?>
+                        <?php
+                        $subId = $totSubCat->subcategory->detID;
+                        array_push($subTotId,$subId);
+                        ?>
 
-                                        <?php $i = $loop->index ?>
-
-                                    @foreach($budgetDetails as $item)
-                                        @foreach($item->subcategory as $item1)
-
-                                                @foreach($item1->gl_codes->whereIn('templateDetailID', $subTotId) as $item2)
-
-                                                    <?php $totBudget += $item2->items[$i]->budjetAmtRpt ?>
-                                                    @endforeach
-                                            @endforeach
-                                        @endforeach
-                                            <?php $totYearBudget += $totBudget ?>
-
-                                            <td>{{ number_format($totBudget,2) }}</td>
-                                    @endforeach
-                                        <td>{{ number_format($totYearBudget,2) }}</td>
-
-                         @endif
-
-
-
-
-
-
-
-               @if($item->itemType == 1)
-                       @foreach($item->subcategory as $item1)
-                            @php $isFinal = $item1->isFinalLevel @endphp
-                            @php $isItemType = $item1->itemType @endphp
-
-            <tr>
-                           <td>{{ $mainNo }}.{{ $loop->index + 1 }}</td>
-                           <td>{{ $item1->description }}</td>
-
-
-
-                <?php $subFinalTotId = array(); ?>
-
-                @foreach($item->subcategory as $totSubCat)
-                    @php $isFinal = $totSubCat->isFinalLevel @endphp
-                    @php $isType = $totSubCat->itemType @endphp
-                    @if($isFinal == 1 && $isType == 3)
-
-                        @foreach($totSubCat->gllink as $finalCat)
-                            <?php
-
-                            $subIdFinal = $finalCat->subCategory;
-                            array_push($subFinalTotId,$subIdFinal);
-                            ?>
-                        @endforeach
-                    @endif
                 @endforeach
-                           <?php $totBudget = 0 ?>
-                           <?php $totYear = 0 ?>
+            @endif
+
+
+
+            @if($type == 3)
+
+                    <?php $totBudget = 0 ?>
+                    <?php $totYearBudget = 0 ?>
+                @foreach ($months as $month)
+                        <?php $totBudget = 0 ?>
+
+                        <?php $i = $loop->index ?>
+
+                    @foreach($budgetDetails as $item)
+                        @foreach($item->subcategory as $item1)
+
+                            @foreach($item1->gl_codes->whereIn('templateDetailID', $subTotId) as $item2)
+
+                                    <?php $totBudget += $item2->items[$i]->budjetAmtRpt ?>
+                            @endforeach
+                        @endforeach
+                    @endforeach
+                        <?php $totYearBudget += $totBudget ?>
+
+                    <td>{{ number_format($totBudget,2) }}</td>
+                @endforeach
+                <td>{{ number_format($totYearBudget,2) }}</td>
+
+            @endif
+
+
+        @if($item->itemType == 1)
+            @foreach($item->subcategory as $key => $item1)
+                @php $isFinal = $item1->isFinalLevel @endphp
+                @php $isItemType = $item1->itemType @endphp
+
+                <tr>
+                    <td>{{ $mainNo }}.{{ $loop->index + 1 }}</td>
+                    <td>{{ $item1->description }}</td>
+
+
+                    @if($isFinal == 0 && $isItemType == 2)
+                       @foreach($item1->subcategory as $key2 => $subCategory)
+                        <tr>
+                            <td>{{ $mainNo }}.{{ $key + 1 }}.{{$key2+1}}</td>
+                            <td>{{ $subCategory->description }}</td>
+                            @foreach($months as $month)
+                                <td>0</td>
+                            @endforeach
+                            <td>0</td>
+                        </tr>
+
+                        @if($subCategory->isFinalLevel == 0 && $subCategory->itemType == 2)
+                            @foreach($subCategory->subcategory as $key3 => $subSubCategory)
+                                <tr>
+                                    <td>{{ $mainNo }}.{{ $key + 1 }}.{{$key2+1}}.{{$key3+1}}</td>
+                                    <td>{{ $subSubCategory->description }}</td>
+                                    @foreach($months as $month)
+                                        <td>0</td>
+                                    @endforeach
+                                    <td>0</td>
+                                </tr>
+
+                                @if($subSubCategory->isFinalLevel == 0 && $subSubCategory->itemType == 2)
+                                    @foreach($subSubCategory->subcategory as $key4 => $subSubSubCategory)
+                                        <tr>
+                                            <td>{{ $mainNo }}.{{ $key + 1 }}.{{$key2+1}}.{{$key3+1}}.{{$key4+1}}</td>
+                                            <td>{{ $subSubSubCategory->description }}</td>
+                                            @foreach($months as $month)
+                                                <td>0</td>
+                                            @endforeach
+                                            <td>0</td>
+                                        </tr>
+
+                                        @if($subSubSubCategory->isFinalLevel == 0 && $subSubSubCategory->itemType == 2)
+                                            @foreach($subSubSubCategory->subcategory as $key5 => $subSubSubSubCategory)
+                                                <tr>
+                                                    <td>{{ $mainNo }}.{{ $key + 1 }}.{{$key2 + 1}}.{{$key3 + 1}}.{{$key4 + 1}}.{{$key5 + 1}}</td>
+                                                    <td>{{ $subSubSubSubCategory->description }}</td>
+                                                    @foreach($months as $month)
+                                                        <td>0</td>
+                                                    @endforeach
+                                                    <td>0</td>
+                                                </tr>
+                                            @endforeach
+                                        @endif
+
+                                    @endforeach
+                                @endif
+                            @endforeach
+                        @endif
+                       @endforeach
+                    @else
+                        <?php $subFinalTotId = array(); ?>
+
+                        @foreach($item->subcategory as $totSubCat)
+                            @php $isFinal = $totSubCat->isFinalLevel @endphp
+                            @php $isType = $totSubCat->itemType @endphp
+                            @if($isFinal == 1 && $isType == 3)
+
+                                @foreach($totSubCat->gllink as $finalCat)
+                                        <?php
+
+                                        $subIdFinal = $finalCat->subCategory;
+                                        array_push($subFinalTotId,$subIdFinal);
+                                        ?>
+                                @endforeach
+                            @endif
+                        @endforeach
+                            <?php $totBudget = 0 ?>
+                            <?php $totYear = 0 ?>
                             <?php $totYearBudget = 0 ?>
 
-                            @if($isFinal == 1 && $isItemType == 3)
+                        @if($isFinal == 1 && $isItemType == 3)
+                                <?php $totBudget = 0 ?>
+                            @foreach ($months as $month)
                                     <?php $totBudget = 0 ?>
-                                    @foreach ($months as $month)
-                                        <?php $totBudget = 0 ?>
 
-                                        <?php $i = $loop->index ?>
+                                    <?php $i = $loop->index ?>
 
-                                        @foreach($budgetDetails as $item)
-                                            @foreach($item->subcategory as $item1)
+                                @foreach($budgetDetails as $item)
+                                    @foreach($item->subcategory as $item1)
 
-                                                @foreach($item1->gl_codes->whereIn('templateDetailID', $subFinalTotId) as $item2)
+                                        @foreach($item1->gl_codes->whereIn('templateDetailID', $subFinalTotId) as $item2)
 
-                                                    <?php $totBudget += $item2->items[$i]->budjetAmtRpt ?>
-                                                @endforeach
-                                            @endforeach
+                                                <?php $totBudget += $item2->items[$i]->budjetAmtRpt ?>
                                         @endforeach
-                                            <?php $totYearBudget += $totBudget ?>
-                                            <td>{{ number_format($totBudget,2) }}</td>
                                     @endforeach
-                                        <td>{{ number_format($totYearBudget,2) }}</td>
+                                @endforeach
+                                    <?php $totYearBudget += $totBudget ?>
+                                <td>{{ number_format($totBudget,2) }}</td>
+                            @endforeach
+                            <td>{{ number_format($totYearBudget,2) }}</td>
 
-                            @endif
-
-
-
-                          @if($isFinal == 1 && $isItemType == 2)
-                           @foreach ($months as $month)
-                               <?php $totBudget = 0 ?>
-                               <?php $i = $loop->index ?>
-                               @foreach($item1->gl_codes as $item2)
-                                    <?php $totBudget += $item2->items[$i]->budjetAmtRpt ?>
-                               @endforeach
-                                   <?php $totYear +=  $totBudget ?>
-                                   <td>{{ number_format($totBudget,2) }}</td>
-                           @endforeach
-                           <td>{{ number_format($totYear,2) }}</td>
-                            @endif
-                       @foreach($item1->gl_codes as $item2)
-                               <tr>
-                                     <td></td>
-                                     <td>
-                                         <strong></strong><strong>{{ $item2->glDescription }} | {{ $item2->glCode }}</strong>
-                                     </td>
-                                   <?php $totBudgetYear = 0 ?>
-                                   @foreach($item2->items as $item3)
-                                    <?php   $totBudgetYear += $item3->budjetAmtRpt ?>
-                                       <td>{{ number_format($item3->budjetAmtRpt,2) }}</td>
-                                   @endforeach
-                                       <td>{{ number_format($totBudgetYear,2) }}</td>
-
-                               </tr>
-                           @endforeach
-                       </tr>
-                   @endforeach
-               @endif
+                        @endif
 
 
-           </tr>
 
-@endforeach
-   </tbody>
+                        @if($isFinal == 1 && $isItemType == 2)
+                            @foreach ($months as $month)
+                                    <?php $totBudget = 0 ?>
+                                    <?php $i = $loop->index ?>
+                                @foreach($item1->gl_codes as $item2)
+                                        <?php $totBudget += $item2->items[$i]->budjetAmtRpt ?>
+                                @endforeach
+                                    <?php $totYear +=  $totBudget ?>
+                                <td>{{ number_format($totBudget,2) }}</td>
+                            @endforeach
+                            <td>{{ number_format($totYear,2) }}</td>
+                        @endif
+                        @foreach($item1->gl_codes as $item2)
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <strong></strong><strong>{{ $item2->glDescription }} | {{ $item2->glCode }}</strong>
+                                </td>
+                                    <?php $totBudgetYear = 0 ?>
+                                @foreach($item2->items as $item3)
+                                        <?php   $totBudgetYear += $item3->budjetAmtRpt ?>
+                                    <td>{{ number_format($item3->budjetAmtRpt,2) }}</td>
+                                @endforeach
+                                <td>{{ number_format($totBudgetYear,2) }}</td>
+
+                            </tr>
+                            @endforeach
+                    @endif
+
+                    </tr>
+                    @endforeach
+                    @endif
+                    </tr>
+
+                    @endforeach
+                    </tbody>
 </table>
 </html>
