b7b4643836a6e18f3bcda18999f747901961fbb7
Safeena Ansar <45002898+safeena-1@users.noreply.github.com>
2024-10-30 15:31:15 +0530

    chore(accounts receivable): Fixed customer ledger email timeout issue [GCP-3841] (#7023)

--- app/Jobs/SentCustomerLedgerSubJob.php
+++ app/Jobs/SentCustomerLedgerSubJob.php
@@ -5,7 +5,6 @@ namespace App\Jobs;
 use App\helper\CommonJobService;
 use App\Http\Controllers\API\AccountsReceivableReportAPIController;
 use App\Http\Controllers\AppBaseController;
-use App\Models\Company;
 use App\Models\CustomerContactDetails;
 use App\Models\CustomerMaster;
 use Illuminate\Bus\Queueable;
@@ -16,7 +15,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
-use ZipArchive;
 
 class SentCustomerLedgerSubJob implements ShouldQueue
 {
@@ -61,145 +59,43 @@ class SentCustomerLedgerSubJob implements ShouldQueue
         $customerMaster = CustomerMaster::find($customerCodeSystem);
         $fetchCusEmail = CustomerContactDetails::where('customerID', $customerCodeSystem)->get();
         if (!$fetchCusEmail->isEmpty()) {
-            self::generateReportView($input, $customerCodeSystem, $fetchCusEmail, $customerMaster->CustomerName);
+            $reportTypeID = $input['reportTypeID'];
+            $baseController = app()->make(AppBaseController::class);
+            $databaseName = $this->db ?? 'local';
+            $path = public_path() . '/uploads/emailAttachment/customer_ledger_' . $databaseName . '_' . $input['companySystemID'] . '_' . $customerCodeSystem;
+            if (!file_exists($path)) {
+                File::makeDirectory($path, 0777, true, true);
+            }
+            if ($reportTypeID == 'CLT1') { //customer ledger template 1
+                $request = (object)$baseController->convertArrayToSelectedValue($input, array('currencyID'));
+                $output = $this->receivableController->getCustomerLedgerTemplate1QRY($request);
+                if($output) {
+                    $reportCount = 1;
+                    $outputChunkData = collect($output)->chunk(300);
+                    foreach ($outputChunkData as $recordsChuncked) {
+                        SentCustomerLedgerPdfGeneration::dispatch($db, $input, $reportCount, $recordsChuncked, count($outputChunkData), $path, $customerCodeSystem, $fetchCusEmail, $customerMaster->CustomerName);
+                        $reportCount++;
+                    }
+                } else {
+                    Log::error('No details found');
+                }
+            } else {
+                $request = (object)$baseController->convertArrayToSelectedValue($input, array('currencyID'));
+
+                $output = $this->receivableController->getCustomerLedgerTemplate2QRY($request);
+                if($output) {
+                    $reportCount = 1;
+                    $outputChunkData = collect($output)->chunk(300);
+                    foreach ($outputChunkData as $recordsChuncked) {
+                        SentCustomerLedgerPdfGeneration::dispatch($db, $input, $reportCount, $recordsChuncked, count($outputChunkData), $path, $customerCodeSystem, $fetchCusEmail, $customerMaster->CustomerName);
+                        $reportCount++;
+                    }
+                } else {
+                    Log::error('No details found');
+                }
+            }
         } else {
             Log::error("Customer email is not updated for " . $customerMaster->CustomerName . ". report is not sent");
-        }
-    }
-
-    private function generateReportView($request, $customerCodeSystem, $fetchCusEmail, $customerName)
-    {
-        $reportTypeID = $request['reportTypeID'];
-        $baseController = app()->make(AppBaseController::class);
-
-        $checkIsGroup = Company::find($request['companySystemID']);
-        $databaseName = $this->db ?? 'local';
-        $path = public_path() . '/uploads/emailAttachment/customer_ledger_' . $databaseName . '_' . $request['companySystemID'] . '_' . $customerCodeSystem;
-        if (!file_exists($path)) {
-            File::makeDirectory($path, 0777, true, true);
-        }
-        $recordExists = 0;
-        if ($reportTypeID == 'CLT1') { //customer ledger template 1
-            $request = (object)$baseController->convertArrayToSelectedValue($request, array('currencyID'));
-            $companyLogo = $checkIsGroup->logo_url;
-
-            $output = $this->receivableController->getCustomerLedgerTemplate1QRY($request);
-            if($output) {
-                $recordExists = 1;
-                $outputChunkData = collect($output)->chunk(300);
-                $reportCount = 1;
-
-                foreach ($outputChunkData as $recordsChuncked) {
-                    $outputArr = array();
-                    $invoiceAmount = collect($recordsChuncked)->pluck('invoiceAmount')->toArray();
-                    $invoiceAmount = array_sum($invoiceAmount);
-
-                    $paidAmount = collect($recordsChuncked)->pluck('paidAmount')->toArray();
-                    $paidAmount = array_sum($paidAmount);
-
-                    $balanceAmount = collect($recordsChuncked)->pluck('balanceAmount')->toArray();
-                    $balanceAmount = array_sum($balanceAmount);
-
-                    $decimalPlace = collect($recordsChuncked)->pluck('balanceDecimalPlaces')->toArray();
-                    $decimalPlace = array_unique($decimalPlace);
-
-                    if ($recordsChuncked) {
-                        foreach ($recordsChuncked as $val) {
-                            $outputArr[$val->concatCustomerName][$val->documentCurrency][] = $val;
-                        }
-                    }
-                    $dataArr = array('reportData' => $outputArr, 'companyName' => $checkIsGroup->CompanyName, 'balanceAmount' => $balanceAmount, 'currencyDecimalPlace' => !empty($decimalPlace) ? $decimalPlace[0] : 2, 'paidAmount' => $paidAmount, 'invoiceAmount' => $invoiceAmount, 'fromDate' => \Helper::dateFormat($request->fromDate),'companyLogo' => $companyLogo);
-
-                    /*** make pdf file */
-                    $html = view('print.customer_ledger_template_one', $dataArr)->render();
-                    $pdf = \App::make('dompdf.wrapper');
-                    $pdf->loadHTML($html)->setPaper('a4', 'landscape')->save($path.'/customer_ledger_' . $customerCodeSystem . '_' . $reportCount . '.pdf');
-
-                    $reportCount++;
-                }
-            } else {
-                Log::error('No details found');
-            }
-        } else {
-            $request = (object)$baseController->convertArrayToSelectedValue($request, array('currencyID'));
-            $companyLogo = $checkIsGroup->logo_url;
-
-            $output = $this->receivableController->getCustomerLedgerTemplate2QRY($request);
-            if($output) {
-                $recordExists = 1;
-                $outputChunkData = collect($output)->chunk(300);
-                $reportCount = 1;
-
-                foreach ($outputChunkData as $recordsChuncked) {
-                    $outputArr = array();
-                    $invoiceAmount = collect($recordsChuncked)->pluck('invoiceAmount')->toArray();
-                    $invoiceAmount = array_sum($invoiceAmount);
-
-                    $decimalPlace = collect($recordsChuncked)->pluck('balanceDecimalPlaces')->toArray();
-                    $decimalPlace = array_unique($decimalPlace);
-
-                    if ($recordsChuncked) {
-                        foreach ($recordsChuncked as $val) {
-                            $outputArr[$val->concatCustomerName][$val->documentCurrency][] = $val;
-                        }
-                    }
-                    $dataArr = array('reportData' => $outputArr, 'companyName' => $checkIsGroup->CompanyName, 'currencyDecimalPlace' => !empty($decimalPlace) ? $decimalPlace[0] : 2, 'invoiceAmount' => $invoiceAmount, 'fromDate' => \Helper::dateFormat($request->fromDate), 'toDate' => \Helper::dateFormat($request->toDate), 'companyLogo' => $companyLogo);
-
-                    $html = view('print.customer_ledger_template_two', $dataArr)->render();
-                    $pdf = \App::make('dompdf.wrapper');
-                    $pdf->loadHTML($html)->setPaper('a4', 'landscape')->save($path . '/customer_ledger_' . $customerCodeSystem . '_' . $reportCount . '.pdf');
-
-                    $reportCount++;
-                }
-            } else {
-                Log::error('No details found');
-            }
-        }
-
-        if($recordExists == 1) {
-            $zipFilePath = public_path('uploads/emailAttachment/customer_ledger_' . $customerCodeSystem . '.zip');
-            $zip = new ZipArchive;
-
-            if ($zip->open($zipFilePath, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
-                foreach (glob($path . '/*.pdf') as $file) {
-                    $zip->addFile($file, basename($file));
-                }
-                $zip->close();
-                File::deleteDirectory($path);
-            } else {
-                Log::error('Failed to create zip file: ' . $zipFilePath);
-            }
-
-            $company = Company::where('companySystemID', $request->companySystemID)->first();
-
-            $footer = "<font size='1.5'><i><p><br><br><br>SAVE PAPER - THINK BEFORE YOU PRINT!" .
-                "<br>This is an auto generated email. Please do not reply to this email because we are not " .
-                "monitoring this inbox.</font>";
-
-            $emailSentTo = 0;
-            foreach ($fetchCusEmail as $row) {
-                if (!empty($row->contactPersonEmail)) {
-                    $emailSentTo = 1;
-                    $dataEmail['empEmail'] = $row->contactPersonEmail;
-                    $dataEmail['companySystemID'] = $request->companySystemID;
-
-                    $temp = "Dear " . $customerName . ',<p> Customer ledger report has been sent from ' . $company->CompanyName . $footer;
-
-                    $dataEmail['isEmailSend'] = 0;
-                    $dataEmail['attachmentFileName'] = $zipFilePath;
-                    $dataEmail['alertMessage'] = "Customer ledger report from " . $company->CompanyName;
-                    $dataEmail['emailAlertMessage'] = $temp;
-                    $sendEmail = \Email::sendEmailErp($dataEmail);
-                    if (!$sendEmail["success"]) {
-                        Log::error($sendEmail["message"]);
-                    } else {
-                        Log::error('Customer ledger email sent successfully');
-                    }
-                }
-            }
-            if($emailSentTo == 0) {
-                Log::error('Email not sent.');
-            }
         }
     }
 }
