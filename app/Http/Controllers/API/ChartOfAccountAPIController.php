b9c3582199dd0609ca868e9386cd357d0e71824e
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2024-11-27 15:33:31 +0530

    chore(system admin): there can only be one active Retained Earnings account [GCP-5429] (#7195)

--- app/Http/Controllers/API/ChartOfAccountAPIController.php
+++ app/Http/Controllers/API/ChartOfAccountAPIController.php
@@ -409,6 +409,7 @@ class ChartOfAccountAPIController extends AppBaseController
                     $retainEarningCode = $isRetainedEarningExists->first();
                     return $this->sendError('There is an Retained Earnings Account, that is already defined '.$retainEarningCode->AccountCode.' - '.$retainEarningCode->AccountDescription,500);
                 }
+
                 $availability = FALSE;
                 while (!$availability) {
                     $accountCode = DocumentCodeGenerate::generateAccountCode($input['reportTemplateCategory'])['data'];
