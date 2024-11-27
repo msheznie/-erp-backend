fb0843101e78d7cc820019c507694dcf6ae84008
Jayan Pradeep Anuranga <89915371+jayananuranga@users.noreply.github.com>
2024-11-27 10:28:27 +0530

    feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916] (#7189)
    * feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916]
    
    * feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916]
    
    * feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916]
    
    * feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916]
    
    * feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916]
    
    * feat(SRM) View the previously opened Tender and RFX Details [GSUP-1916]
    
    ---------
    
    Co-authored-by: jayan anuranga <jayan.anuranga@osos.om>

--- app/Models/TenderMaster.php
+++ app/Models/TenderMaster.php
@@ -737,4 +737,12 @@ class TenderMaster extends Model
         return $this->hasOne('App\Models\SrmTenderPo', 'tender_id', 'id')->where('status', 1);
     }
 
+    public static function getTenderDidOpeningDates($tenderId, $companyId)
+    {
+        return TenderMaster::select('id','stage', 'bid_opening_date',
+            'technical_bid_opening_date', 'bid_opening_end_date', 'technical_bid_closing_date')
+            ->where('id', $tenderId)
+            ->where('company_id', $companyId)
+            ->first();
+    }
 }
