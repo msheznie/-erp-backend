db2d0287519afc6e40fb16e703c618beb7859efa
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2024-12-19 10:56:10 +0530

    chore(budget): exchange rate issue on customer ledger fixed [GCP-5633] (#7349)
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- app/Http/Controllers/API/AccountsReceivableReportAPIController.php
+++ app/Http/Controllers/API/AccountsReceivableReportAPIController.php
@@ -7662,6 +7662,7 @@ AND erp_generalledger.documentTransAmount > 0 AND erp_generalledger.supplierCode
 
                 $db = isset($request->db) ? $request->db : "";
 
+
                 $employeeID = \Helper::getEmployeeSystemID();
                 AccountsReceivablePdfJob::dispatch($db, $request, [$employeeID])->onQueue('reporting');
 
