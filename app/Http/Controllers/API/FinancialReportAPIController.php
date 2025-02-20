d07d35acc553ab4c0c794911e25d59bbc07c8548
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2025-02-20 09:48:21 +0530

    chore(system admin) : The other changes GL Drill Down loading for a long time [GCP-6240] (#7588)

--- app/Http/Controllers/API/FinancialReportAPIController.php
+++ app/Http/Controllers/API/FinancialReportAPIController.php
@@ -10863,7 +10863,6 @@ SELECT SUM(amountLocal) AS amountLocal,SUM(amountRpt) AS amountRpt FROM (
         $fromDate = $fromDate->format('Y-m-d');
         $selectedGL = $request->details; 
         $selectedColumn = $request->selectedColumn; 
-        $selectedColumn = str_replace(' ', '_', $selectedColumn);
         $toDate = new Carbon($request->toDate);
         $toDate = $toDate->format('Y-m-d');
         $currency = isset($request->currency[0]) ? $request->currency[0]: $request->currency;
@@ -11036,5 +11035,4 @@ SELECT SUM(amountLocal) AS amountLocal,SUM(amountRpt) AS amountRpt FROM (
         }
         return $type;
     }
-    
 }
