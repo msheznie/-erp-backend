8022e53a8c980d1bbd19b48d31b8dcbcd4130a51
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2025-03-04 07:59:34 +0530

    chore(treasury management): Bank Config model table changed to public and fillable fields changes to [GCP-6273] (#7667)
    * chore(treasury management): issue on db switch fixed on bankstatus job [GCP-6273]
    
    * chore(treasury management): switch db issue on b2b config services fixed [GCP-6273-A]
    
    * chore(treasury management): db switch issue fixed with new service to job [GCP-6273-A]
    
    * chore(treasury mangement): changes on notification function [GCP-6273]
    
    * chore(treasury management): service logic written on controller b2b [GCP-6273]
    
    * chore(treasury management): b2b config service refering hrms table fixes [GCP-6273]
    
    * chore(tresaury management): model table changed to public and fillable fields changes to [GCP-6273]
    
    ---------
    
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- app/Models/BankConfig.php
+++ app/Models/BankConfig.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class BankConfig extends Model
 {
 
-    protected $table = 'bank_configs';
+    public $table = 'bank_configs';
 
     protected $primaryKey = 'uuid';
     public $incrementing = false;
     protected $keyType = 'string';
 
-    protected $fillable = [
+    public $fillable = [
         'uuid',
         'slug',
         'bank_master_id',
