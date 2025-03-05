f2665d2ffd4d57fd4ca5da0a5f7dfcbc86367849
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2025-03-05 16:05:42 +0530

    chore(procurement): Unable to search purchase requests in approval approved page  [GCP-6308] (#7674)

--- app/Http/Controllers/API/PurchaseRequestAPIController.php
+++ app/Http/Controllers/API/PurchaseRequestAPIController.php
@@ -1331,6 +1331,8 @@ class PurchaseRequestAPIController extends AppBaseController
         });
 
         return \DataTables::of($purchaseRequests)
+            ->filter(function ($instance){  
+            })
             ->order(function ($query) use ($input) {
                 if (request()->has('order')) {
                     if ($input['order'][0]['column'] == 0) {
