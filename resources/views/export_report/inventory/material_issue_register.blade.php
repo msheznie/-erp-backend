e59eb6d76dc1d736cb1c1083b560b005b03c7d21
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2025-06-12 12:49:14 +0530

    chore(inventory): material issue report excel template added [GCP-7803] (#8219)
    * chore(inventory): segment based report added [GCP-7803]
    
    * export excel
    
    * code review changes
    
    * tax total query changes
    
    * revert finanicalReportContorller chanfges
    
    * chore(inventory): material issue report excel template added [GCP-7803]
    
    * changes
    
    ---------
    
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

---
+++ resources/views/export_report/inventory/material_issue_register.blade.php
@@ -0,0 +1,115 @@
+<html>
+<center>
+    <table>
+        <thead>
+        <tr></tr>
+        <tr>
+            <td colspan="2"></td>
+            <td><h1>Asset Expense Report</h1></td>
+        </tr>
+        <tr></tr>
+        <tr style="font-weight: bold">
+            <td>Report Filter</td>
+            <td>Start Date : {{ $fromDate }} </td>
+            <td>End Date :  {{ $toDate }}</td>
+            <td>Type :
+                @if($reportType == 1)
+                    Employee
+                @elseif($reportType == 2)
+                    Assets
+                @else
+                    Segments
+                @endif
+            </td>
+
+            @if($reportType == 2)
+                <td>Assets : {{$selectedAssets ?? 'Not Selected'}}</td>
+            @endif
+
+            @if($reportType == 3)
+                <td>Segments : {{$selectedSegments ?? 'Not Selected'}} </td>
+            @endif
+            <td>
+
+                @if($reportType == 2)
+                    <span>Group By Asset :
+                        @if(empty($groupBy))
+                            NO
+                        @else
+                            YES
+                        @endif
+                    </span>
+                @endif
+
+                @if($reportType == 3)
+                        <span>Group By Segment :
+                            @if(empty($groupBy))
+                                NO
+                            @else
+                                YES
+                            @endif
+                        </span>
+                @endif
+            </td>
+
+
+        </tr>
+        <tr></tr>
+        <tr>
+            <th class="text-center">Issue Code</th>
+            <th class="text-center">Issue Date</th>
+            <th class="text-center">Request No</th>
+            <th class="text-center">Item Code</th>
+            <th class="text-center">Item Description</th>
+            <th class="text-center">UOM</th>
+            <th class="text-center">Issued Qty</th>
+            @if($reportType == 1 || $reportType == 2)
+                <th class="text-center">Issued To - @if($reportType == 2) Asset Code @else Item Code @endif</th>
+                <th class="text-center">Issued To - @if($reportType == 2) Asset Description @else Item Description @endif</th>
+            @else
+                <th class="text-center">Segment</th>
+            @endif
+            <th class="text-center">Qty</th>
+            <th class="text-center">Cost</th>
+            <th class="text-center">Amount</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($reportData->groupedResults as $key => $groupedData)
+                <tr>
+                    @if(empty($groupByAsset))
+                        <td colspan="11">Item Code : {{$key}}</td>
+                    @elseif($groupByAsset && $reportType == 2)
+                        <td colspan="11">Asset Code : {{$groupedData[0]->expenseAllocations[0]->empID ?? NULL}}</td>
+                    @else
+                        <td colspan="11">Segment : {{$groupedData[0]->expenseAllocations[0]->empID ?? NULL}}</td>
+                    @endif
+                </tr>
+
+                @foreach($groupedData as $item)
+                  <tr>
+                      <td>{{$item->itemIssueCode}}</td>
+                      <td>{{$item->issueDate}}</td>
+                      <td>{{$item->RequestCode}}</td>
+                      <td>{{$item->itemPrimaryCode}}</td>
+                      <td>{{$item->itemDescription}}</td>
+                      <td>{{$item->unit}}</td>
+                      <td>{{$item->qtyIssued}}</td>
+                      <td>{{$item->expenseAllocations[0]->empID ?? NULL}}</td>
+                      @if($reportType == 1 || $reportType == 2)
+                      <td>{{$item->expenseAllocations[0]->empName ?? NULL}}</td>
+                      @endif
+                      <td>{{optional($item->expenseAllocations[0])->assignedQty}}</td>
+                      <td>{{optional($item->expenseAllocations[0])->amount}}</td>
+                      <td>{{optional($item->expenseAllocations[0])->assignedQty * optional($item->expenseAllocations[0])->amount}}</td>
+                  </tr>
+                @endforeach
+
+                <tr>
+                    <td colspan="11"></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</center>
+</html>
