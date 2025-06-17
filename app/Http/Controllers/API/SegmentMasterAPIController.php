6f7f2c58ff3aca4294ca08940c9fa39d07c43ed5
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2025-06-17 22:46:42 +0530

    chore(system admin): Segment status update [GCP-8409] (#8251)

--- app/Http/Controllers/API/SegmentMasterAPIController.php
+++ app/Http/Controllers/API/SegmentMasterAPIController.php
@@ -194,7 +194,7 @@ class SegmentMasterAPIController extends AppBaseController
     {
         /** @var SegmentMaster $segmentMaster */
         $segmentMaster = $this->segmentMasterRepository->withoutGlobalScope('final_level')
-            ->with(['approved_by_emp','company'])
+            ->with(['approved_by_emp','company','parent'])
             ->withcount(['sub_levels'])->find($id);
 
         if (empty($segmentMaster)) {
