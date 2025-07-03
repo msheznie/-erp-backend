013803fd22db8e4fd63f6541409a5bc73ada052e
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2025-07-03 10:30:41 +0530

    chore(common): currency exchange rate division by zero issue fixes [GCP-8400] (#8349)
    * chore(common): currency exchange rate division by zero issue fixes [GCP-8400]
    
    * changes
    
    ---------
    
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- app/helper/general_helper.php
+++ app/helper/general_helper.php
@@ -3763,10 +3763,12 @@ class Helper
                             $reportingAmount = $transactionAmount * $trasToRptER;
                         }
                     } else {
+
+
                         if ($trasToRptER > 1) {
                             $reportingAmount = $transactionAmount * $trasToRptER;
                         } else {
-                            $reportingAmount = $transactionAmount / $trasToRptER;
+                            $reportingAmount = $trasToRptER != 0 ? $transactionAmount / $trasToRptER : 1;
                         }
                     }
                 }
@@ -3784,7 +3786,7 @@ class Helper
                         if ($trasToLocER > 1) {
                             $localAmount = $transactionAmount * $trasToLocER;
                         } else {
-                            $localAmount = $transactionAmount / $trasToLocER;
+                            $localAmount = $trasToLocER  != 0 ? $transactionAmount / $trasToLocER : 1;
                         }
                     }
                 }
@@ -3812,7 +3814,7 @@ class Helper
                         if ($transToBankER > 1) {
                             $bankAmount = $transactionAmount * $transToBankER;
                         } else {
-                            $bankAmount = $transactionAmount / $transToBankER;
+                            $bankAmount = $transToBankER != 0 ? $transactionAmount / $transToBankER : 1;
                         }
                     }
                 }
@@ -3837,7 +3839,7 @@ class Helper
                     if ($transToDocER > 1) {
                         $documentAmount = $transactionAmount * $transToDocER;
                     } else {
-                        $documentAmount = $transactionAmount / $transToDocER;
+                        $documentAmount = $transToDocER != 0 ? $transactionAmount / $transToDocER : 1;
                     }
                 }
             }
