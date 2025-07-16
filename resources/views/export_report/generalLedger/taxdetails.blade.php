8db39b50e1c87d48057c65369b2d0f9f95ed28af
ThatSaravanan1112 <88361609+ThatSaravanan1112@users.noreply.github.com>
2025-07-16 20:42:02 +0530

    chore(general ledger): sentry issue fixes on documentTypeID [GCP-9133] (#8421)
    Co-authored-by: ThatSaravanan1112 <thatparanthan.saravanan@osos.om>

--- resources/views/export_report/generalLedger/taxdetails.blade.php
+++ resources/views/export_report/generalLedger/taxdetails.blade.php
@@ -265,7 +265,7 @@
                             }
                         } else {
                             if (in_array($tempType, [1, 2])) {
-                                $totalAmount = $data->value - $data->discount + ((isset($data->rcmActivated) && $data->rcmActivated) ? 0 : $data->VATAmount) + (in_array($data->documentTypeID,[0,2]) ? $data->exempt_vat_portion : 0) - ($data->retentionAmount ?? 0);
+                                $totalAmount = $data->value - $data->discount + ((isset($data->rcmActivated) && $data->rcmActivated) ? 0 : $data->VATAmount) + (isset($data->documentTypeID) && in_array($data->documentTypeID,[0,2]) ? $data->exempt_vat_portion : 0) - ($data->retentionAmount ?? 0);
                             } else {
                                 $totalAmount = $data->value + $data->discount + $data->VATAmount;
                             }
@@ -294,7 +294,7 @@
                             } else {
                                 if (in_array($tempType, [1, 2])) {
                                     $reportingVal = (
-                                        ($data->value - $data->discount + ((isset($data->rcmActivated) && $data->rcmActivated) ? 0 : $data->VATAmount) + (in_array($data->documentTypeID,[0,2]) ? $data->exempt_vat_portion : 0) - ($data->retentionAmount ?? 0))/
+                                        ($data->value - $data->discount + ((isset($data->rcmActivated) && $data->rcmActivated) ? 0 : $data->VATAmount) + (isset($data->documentTypeID) && in_array($data->documentTypeID,[0,2]) ? $data->exempt_vat_portion : 0) - ($data->retentionAmount ?? 0))/
                                         $data->companyReportingER
                                     );
                                 } else {
