b6f2f85d7b29e83e455d2d87441f79a7e69794b3
Ishfaq-A <61774663+Ishfaq-A@users.noreply.github.com>
2025-09-09 18:08:10 +0530

    feat(sourcing management): Tender/Rfx list and creation arabization [GSUP-4114] (#8711)

--- resources/lang/en/srm_tender_rfx.php
+++ resources/lang/en/srm_tender_rfx.php
@@ -3,4 +3,7 @@
 return [
     'rfx_title_cannot_be_duplicated' => 'RFX title cannot be duplicated',
     'tender_title_cannot_be_duplicated' => 'Tender title cannot be duplicated',
+    'successfully_saved' => 'Successfully saved',
+    'successfully_deleted' => 'Successfully deleted',
+    'successfully_published' => 'Successfully Published'
 ];
