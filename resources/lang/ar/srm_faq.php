e248a123aad11d9a6325b29f0be28452799ed314
AflalAbdeen <44675108+AflalAbdeen@users.noreply.github.com>
2025-09-09 14:11:11 +0530

    Merge pull request #8708 from pbsgears/GSUP-4092
    feat(Sourcing Management): Sourcing Management Arabic Translation [GS…

--- resources/lang/ar/srm_faq.php
+++ resources/lang/ar/srm_faq.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-
+    'successfully_saved' => 'تم الحفظ بنجاح',
+    'tender_faq_not_found' => 'لم يتم العثور على أسئلة المناقصة الشائعة',
+    'tender_faq_deleted' => 'تم حذف أسئلة المناقصة بنجاح',
 ];
--- resources/lang/en/srm_faq.php
+++ resources/lang/en/srm_faq.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-
+    'successfully_saved' => 'Successfully saved',
+    'tender_faq_not_found' => 'Tender FAQ not found',
+    'tender_faq_deleted' => 'Tender FAQ deleted successfully',
 ];
