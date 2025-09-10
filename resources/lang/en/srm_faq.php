1567b07af7c0a99ba3ec7e2c0fae595efbc43343
AflalAbdeen <44675108+AflalAbdeen@users.noreply.github.com>
2025-09-10 08:48:23 +0530

    chore(Sourcing Management): Sourcing Management Arabic Translation [GSUP-4092] (#8720)

--- resources/lang/en/srm_faq.php
+++ resources/lang/en/srm_faq.php
@@ -4,4 +4,9 @@ return [
     'successfully_saved' => 'Successfully saved',
     'tender_faq_not_found' => 'Tender FAQ not found',
     'tender_faq_deleted' => 'Tender FAQ deleted successfully',
+    "comment_field_is_required" => "Comment field is required.",
+    "successfully_thread_closed"  =>  "Successfully thread closed",
+    "emails_sent_successfully" => "Email/s sent successfully",
+    "not_valid_emails_enter_valid" => "Not valid email/s. Please enter valid email/s.",
+    "emails_already_exist" => "Email/s already exist.",
 ];
