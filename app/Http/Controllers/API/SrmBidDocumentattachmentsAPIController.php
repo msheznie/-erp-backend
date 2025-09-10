2a4c9e36cc1246da3a8db689c7d95f6af02c09c3
AflalAbdeen <44675108+AflalAbdeen@users.noreply.github.com>
2025-09-10 17:59:15 +0530

    chore(Sourcing Management): Sourcing Management Arabic Translation [GSUP-4092] (#8740)
    * chore(Sourcing Management): Sourcing Management Arabic Translation [GSUP-4092]
    
    * chore(Sourcing Management): Sourcing Management Arabic Translation [GSUP-4092]
    
    * chore(Sourcing Management): Sourcing Management Arabic Translation [GSUP-4092]

--- app/Http/Controllers/API/SrmBidDocumentattachmentsAPIController.php
+++ app/Http/Controllers/API/SrmBidDocumentattachmentsAPIController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
+use App\helper\Helper;
 use App\Http\Requests\API\CreateSrmBidDocumentattachmentsAPIRequest;
 use App\Http\Requests\API\UpdateSrmBidDocumentattachmentsAPIRequest;
 use App\Models\SrmBidDocumentattachments;
@@ -18,7 +18,6 @@ use App\Models\CompanyPolicyMaster;
 use App\Models\DocumentMaster;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Finder\SplFileInfo;
-use App\helper\Helper;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 /**
@@ -74,7 +73,7 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
         $this->srmBidDocumentattachmentsRepository->pushCriteria(new FilterTenderDocumentCriteria($request));
         $srmBidDocumentattachments = $this->srmBidDocumentattachmentsRepository->all();
 
-        return $this->sendResponse($srmBidDocumentattachments->toArray(), trans('custom.srm_bid_documentattachments_retrieved_successfully'));
+        return $this->sendResponse($srmBidDocumentattachments->toArray(), 'Srm Bid Documentattachments retrieved successfully');
     }
 
     /**
@@ -128,7 +127,7 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
 
         $srmBidDocumentattachments = $this->srmBidDocumentattachmentsRepository->create($input);
 
-        return $this->sendResponse($srmBidDocumentattachments->toArray(), trans('custom.srm_bid_documentattachments_saved_successfully'));
+        return $this->sendResponse($srmBidDocumentattachments->toArray(), 'Srm Bid Documentattachments saved successfully');
     }
 
     /**
@@ -176,10 +175,10 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
         $srmBidDocumentattachments = $this->srmBidDocumentattachmentsRepository->findWithoutFail($id);
 
         if (empty($srmBidDocumentattachments)) {
-            return $this->sendError(trans('custom.srm_bid_documentattachments_not_found'));
+            return $this->sendError('Srm Bid Documentattachments not found');
         }
 
-        return $this->sendResponse($srmBidDocumentattachments->toArray(), trans('custom.srm_bid_documentattachments_retrieved_successfully'));
+        return $this->sendResponse($srmBidDocumentattachments->toArray(), 'Srm Bid Documentattachments retrieved successfully');
     }
 
     /**
@@ -245,12 +244,12 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
         $srmBidDocumentattachments = $this->srmBidDocumentattachmentsRepository->findWithoutFail($id);
 
         if (empty($srmBidDocumentattachments)) {
-            return $this->sendError(trans('custom.srm_bid_documentattachments_not_found'));
+            return $this->sendError('Srm Bid Documentattachments not found');
         }
 
         $srmBidDocumentattachments = $this->srmBidDocumentattachmentsRepository->update($input, $id);
 
-        return $this->sendResponse($srmBidDocumentattachments->toArray(), trans('custom.srmbiddocumentattachments_updated_successfully'));
+        return $this->sendResponse($srmBidDocumentattachments->toArray(), 'SrmBidDocumentattachments updated successfully');
     }
 
     /**
@@ -298,12 +297,12 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
         $srmBidDocumentattachments = $this->srmBidDocumentattachmentsRepository->findWithoutFail($id);
 
         if (empty($srmBidDocumentattachments)) {
-            return $this->sendError(trans('custom.srm_bid_documentattachments_not_found'));
+            return $this->sendError('Srm Bid Documentattachments not found');
         }
 
         $srmBidDocumentattachments->delete();
 
-        return $this->sendResponse(true, trans('custom.srm_bid_documentattachments_deleted_successfully'));
+        return $this->sendResponse(true, 'Srm Bid Documentattachments deleted successfully');
 
     }
 
@@ -312,106 +311,106 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
         DB::beginTransaction();
         try {
 
-            
-        $input = $request->all();
-        $attachmentDescription = $input['attachmentDescription'];
-        $companySystemID = $input['companySystemID'];
-        $documentSystemID = $input['documentSystemID'];
-        $documentSystemCode = $input['documentSystemCode'];
 
-        $isExist = SrmBidDocumentattachments::where('companySystemID',$companySystemID)
-        ->where('documentSystemID',$documentSystemID)
-        ->where('documentSystemCode',$documentSystemCode)
-        ->where('attachmentDescription',$attachmentDescription)
-        ->count(); 
-        if($isExist >= 1){ 
-           return ['status' => false, 'message' => 'Description already exists'];  
-        }else {
-            
+            $input = $request->all();
+            $attachmentDescription = $input['attachmentDescription'];
+            $companySystemID = $input['companySystemID'];
+            $documentSystemID = $input['documentSystemID'];
+            $documentSystemCode = $input['documentSystemCode'];
 
-            $extension = $input['fileType'];
-
-            $blockExtensions = [
-                'ace', 'ade', 'adp', 'ani', 'app', 'asp', 'aspx', 'asx', 'bas', 'bat', 'cla', 'cer', 'chm', 'cmd', 'cnt', 'com',
-                'cpl', 'crt', 'csh', 'class', 'der', 'docm', 'exe', 'fxp', 'gadget', 'hlp', 'hpj', 'hta', 'htc', 'inf', 'ins', 'isp', 'its', 'jar',
-                'js', 'jse', 'ksh', 'lnk', 'mad', 'maf', 'mag', 'mam', 'maq', 'mar', 'mas', 'mat', 'mau', 'mav', 'maw', 'mda', 'mdb', 'mde', 'mdt',
-                'mdw', 'mdz', 'mht', 'mhtml', 'msc', 'msh', 'msh1', 'msh1xml', 'msh2', 'msh2xml', 'mshxml', 'msi', 'msp', 'mst', 'ops', 'osd',
-                'ocx', 'pl', 'pcd', 'pif', 'plg', 'prf', 'prg', 'ps1', 'ps1xml', 'ps2', 'ps2xml', 'psc1', 'psc2', 'pst', 'reg', 'scf', 'scr',
-                'sct', 'shb', 'shs', 'tmp', 'url', 'vb', 'vbe', 'vbp', 'vbs', 'vsmacros', 'vss', 'vst', 'vsw', 'ws', 'wsc', 'wsf', 'wsh', 'xml',
-                'xbap', 'xnk', 'php'
-            ];
-
-            if (in_array($extension, $blockExtensions)) {
-                return $this->sendError('This type of file not allow to upload.', 500);
-            }
+            $isExist = SrmBidDocumentattachments::where('companySystemID',$companySystemID)
+                ->where('documentSystemID',$documentSystemID)
+                ->where('documentSystemCode',$documentSystemCode)
+                ->where('attachmentDescription',$attachmentDescription)
+                ->count();
+            if($isExist >= 1){
+                return ['status' => false, 'message' => trans('srm_ranking.document_attachments_saved_successfully')];
+            }else {
 
 
-            if (isset($input['sizeInKbs'])) {
-                if ($input['sizeInKbs'] > env('ATTACH_UPLOAD_SIZE_LIMIT')) {
-                    return $this->sendError("Maximum allowed file size is exceeded. Please upload lesser than ".\Helper::bytesToHuman(env('ATTACH_UPLOAD_SIZE_LIMIT')), 500);
+                $extension = $input['fileType'];
+
+                $blockExtensions = [
+                    'ace', 'ade', 'adp', 'ani', 'app', 'asp', 'aspx', 'asx', 'bas', 'bat', 'cla', 'cer', 'chm', 'cmd', 'cnt', 'com',
+                    'cpl', 'crt', 'csh', 'class', 'der', 'docm', 'exe', 'fxp', 'gadget', 'hlp', 'hpj', 'hta', 'htc', 'inf', 'ins', 'isp', 'its', 'jar',
+                    'js', 'jse', 'ksh', 'lnk', 'mad', 'maf', 'mag', 'mam', 'maq', 'mar', 'mas', 'mat', 'mau', 'mav', 'maw', 'mda', 'mdb', 'mde', 'mdt',
+                    'mdw', 'mdz', 'mht', 'mhtml', 'msc', 'msh', 'msh1', 'msh1xml', 'msh2', 'msh2xml', 'mshxml', 'msi', 'msp', 'mst', 'ops', 'osd',
+                    'ocx', 'pl', 'pcd', 'pif', 'plg', 'prf', 'prg', 'ps1', 'ps1xml', 'ps2', 'ps2xml', 'psc1', 'psc2', 'pst', 'reg', 'scf', 'scr',
+                    'sct', 'shb', 'shs', 'tmp', 'url', 'vb', 'vbe', 'vbp', 'vbs', 'vsmacros', 'vss', 'vst', 'vsw', 'ws', 'wsc', 'wsf', 'wsh', 'xml',
+                    'xbap', 'xnk', 'php'
+                ];
+
+                if (in_array($extension, $blockExtensions)) {
+                    return $this->sendError(trans('srm_bid.max_file_type_not_allowed'), 500);
                 }
-            }
-       
-            $input = $this->convertArrayToValue($input);
-            if (isset($input['documentSystemID'])) {
 
-                $documentMaster = DocumentMaster::where('documentSystemID', $input['documentSystemID'])->first();
-                if ($documentMaster) {
-                    $input['documentID'] = $documentMaster->documentID;
+
+                if (isset($input['sizeInKbs'])) {
+                    if ($input['sizeInKbs'] > env('ATTACH_UPLOAD_SIZE_LIMIT')) {
+                        return $this->sendError(trans('srm_bid.max_file_size_exceeded').' '.\Helper::bytesToHuman(env('ATTACH_UPLOAD_SIZE_LIMIT')), 500);
+                    }
                 }
-            }
 
-            $companyID = "";
-            if (isset($input['companySystemID'])) {
+                $input = $this->convertArrayToValue($input);
+                if (isset($input['documentSystemID'])) {
 
-                $companyMaster = Company::where('companySystemID', $input['companySystemID'])->first();
-
-                if ($companyMaster) {
-                    $input['companyID'] = $companyMaster->CompanyID;
-                    $companyID = $companyMaster->CompanyID;
+                    $documentMaster = DocumentMaster::where('documentSystemID', $input['documentSystemID'])->first();
+                    if ($documentMaster) {
+                        $input['documentID'] = $documentMaster->documentID;
+                    }
                 }
+
+                $companyID = "";
+                if (isset($input['companySystemID'])) {
+
+                    $companyMaster = Company::where('companySystemID', $input['companySystemID'])->first();
+
+                    if ($companyMaster) {
+                        $input['companyID'] = $companyMaster->CompanyID;
+                        $companyID = $companyMaster->CompanyID;
+                    }
+                }
+
+
+
+                $input['tender_id'] = $documentSystemCode;
+                $documentAttachments = $this->srmBidDocumentattachmentsRepository->create($input);
+
+                $file = $request->request->get('file');
+                $decodeFile = base64_decode($file);
+
+                $input['myFileName'] = $documentAttachments->companyID . '_' . $documentAttachments->documentID . '_' . $documentAttachments->documentSystemCode . '_' . $documentAttachments->id . '.' . $extension;
+
+                if ($documentAttachments->documentID == 'PRN') {
+                    $documentAttachments->documentID =  $documentAttachments->documentID . 'I';
+                }
+
+
+                if (Helper::checkPolicy($input['companySystemID'], 50)) {
+                    $path = $companyID . '/G_ERP/' . $documentAttachments->documentID . '/' . $documentAttachments->documentSystemCode . '/' . $input['myFileName'];
+                } else {
+                    $path = $documentAttachments->documentID . '/' . $documentAttachments->documentSystemCode . '/' . $input['myFileName'];
+                }
+
+                Storage::disk(Helper::policyWiseDisk($input['companySystemID'], 'public'))->put($path, $decodeFile);
+
+                $input['isUploaded'] = 1;
+                $input['path'] = $path;
+
+
+                $documentAttachments = $this->srmBidDocumentattachmentsRepository->update($input, $documentAttachments->id);
+                DB::commit();
+                Log::info(app()->getLocale());
+                return $this->sendResponse($documentAttachments->toArray(), trans('srm_ranking.document_attachments_saved_successfully'));
             }
 
-
-       
-            $input['tender_id'] = $documentSystemCode;
-            $documentAttachments = $this->srmBidDocumentattachmentsRepository->create($input);
-
-            $file = $request->request->get('file');
-            $decodeFile = base64_decode($file);
-
-            $input['myFileName'] = $documentAttachments->companyID . '_' . $documentAttachments->documentID . '_' . $documentAttachments->documentSystemCode . '_' . $documentAttachments->id . '.' . $extension;
-
-            if ($documentAttachments->documentID == 'PRN') {
-                $documentAttachments->documentID =  $documentAttachments->documentID . 'I';
-            }
-
-
-            if (Helper::checkPolicy($input['companySystemID'], 50)) {
-                $path = $companyID . '/G_ERP/' . $documentAttachments->documentID . '/' . $documentAttachments->documentSystemCode . '/' . $input['myFileName'];
-            } else {
-                $path = $documentAttachments->documentID . '/' . $documentAttachments->documentSystemCode . '/' . $input['myFileName'];
-            }
-
-            Storage::disk(Helper::policyWiseDisk($input['companySystemID'], 'public'))->put($path, $decodeFile);
-
-            $input['isUploaded'] = 1;
-            $input['path'] = $path;
-        
-
-            $documentAttachments = $this->srmBidDocumentattachmentsRepository->update($input, $documentAttachments->id);
-        
-            DB::commit();
-            return $this->sendResponse($documentAttachments->toArray(), trans('custom.document_attachments_saved_successfully'));
-        }
-            
 
         } catch (\Exception $exception) {
             DB::rollBack();
-            return $this->sendError('Unable to upload the attachment', 500);
+            return $this->sendError(trans('srm_ranking.unable_to_upload_the_attachment'), 500);
         }
 
-      
+
     }
 
 
@@ -427,14 +426,14 @@ class SrmBidDocumentattachmentsAPIController extends AppBaseController
         $documentAttachments = $this->srmBidDocumentattachmentsRepository->findWithoutFail($input['id']);
 
         if (empty($documentAttachments)) {
-            return $this->sendError(trans('custom.document_attachments_not_found'));
+            return $this->sendError('Document Attachments not found');
         }
 
         if (!is_null($documentAttachments->path)) {
             if ($exists = Storage::disk(Helper::policyWiseDisk($documentAttachments->companySystemID, 'public'))->exists($documentAttachments->path)) {
                 return Storage::disk(Helper::policyWiseDisk($documentAttachments->companySystemID, 'public'))->download($documentAttachments->path, $documentAttachments->myFileName);
             } else {
-                return $this->sendError(trans('custom.attachments_not_found'), 500);
+                return $this->sendError('Attachments not found', 500);
             }
         } else {
             return $this->sendError('Attachment is not attached', 404);
