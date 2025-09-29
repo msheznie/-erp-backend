1b8a63add8377848cfdc3588e2f92e925e70899c
Arshadgears <86824223+Arshadgears@users.noreply.github.com>
2025-09-29 15:34:23 +0530

    feat(procurement): created suppliercategory (#8977)
    icvsub translation [GCP-15110]

---
+++ app/Models/SupplierCategoryICVSubTranslation.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+
+/**
+ * @SWG\Definition(
+ *      definition="SupplierCategoryICVSubTranslation",
+ *      required={""},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="supCategorySubICVID",
+ *          description="supCategorySubICVID",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="languageCode",
+ *          description="languageCode",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="categoryDescription",
+ *          description="categoryDescription",
+ *          type="string"
+ *      )
+ * )
+ */
+class SupplierCategoryICVSubTranslation extends Model
+{
+    public $table = 'suppliercategoryicvsub_translation';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $primaryKey = 'id';
+
+    public $fillable = [
+        'supCategorySubICVID',
+        'languageCode',
+        'categoryDescription'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'supCategorySubICVID' => 'integer',
+        'languageCode' => 'string',
+        'categoryDescription' => 'string'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    /**
+     * Get the supplier category ICV sub that owns the translation.
+     */
+    public function supplierCategoryICVSub()
+    {
+        return $this->belongsTo('App\Models\SupplierCategoryICVSub', 'supCategorySubICVID', 'supCategorySubICVID');
+    }
+}
+
