35ded090ce1606fbba64864679b6d3e1427b2ecf
ThatSaravanan1112 <thatparanthan.saravanan@osos.om>
2025-10-02 11:21:57 +0530

    qury changes

--- routes/api.php
+++ routes/api.php
@@ -1044,7 +1044,7 @@ Route::group(['middleware' => ['mobileServer']], function () {
         Route::get('updateNotPostedFADepGLEntries', 'GeneralLedgerAPIController@updateNotPostedFADepGLEntries');
         Route::get('updateNotPostedBSIGLEntries', 'GeneralLedgerAPIController@updateNotPostedBSIGLEntries');
         Route::get('updateNotApprovedSegments', 'GeneralLedgerAPIController@updateNotApprovedSegments');
-        Route::get('updateNotPostedAssetEntries', 'GeneralLedgerAPIController@updateNotPostedAssetEntries');
+        // Route::get('updateNotPostedAssetEntries', 'GeneralLedgerAPIController@updateNotPostedAssetEntries');
 
     }
 
