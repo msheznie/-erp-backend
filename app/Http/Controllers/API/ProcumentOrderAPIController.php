9c26e4b4dcb2232d87478ef3ae182edd1cb7cc61
Arshadgears <86824223+Arshadgears@users.noreply.github.com>
2025-10-08 14:46:23 +0530

    feat(procurement): Fixed translation issues PO Export [GCP-15237] (#9037)

--- app/Http/Controllers/API/ProcumentOrderAPIController.php
+++ app/Http/Controllers/API/ProcumentOrderAPIController.php
@@ -5489,7 +5489,7 @@ group by purchaseOrderID,companySystemID) as pocountfnal
         ]);
 
         if ($validator->fails()) {
-            return $this->sendError('Unable to export excel: ' .  $validator->errors()->first());
+            return $this->sendError(trans('custom.unable_to_export_excel') . ': ' .  $validator->errors()->first());
         }
 
         $validator = \Validator::make($input, [
@@ -5497,7 +5497,7 @@ group by purchaseOrderID,companySystemID) as pocountfnal
         ]);
 
         if ($validator->fails()) {
-            return $this->sendError('Unable to export excel: ' .  $validator->errors()->first());
+            return $this->sendError(trans('custom.unable_to_export_excel') . ': ' .  $validator->errors()->first());
         }
 
         $supplierID = $request['supplierID'];
@@ -5748,16 +5748,16 @@ group by purchaseOrderID,companySystemID) as pocountfnal
 
         // return $this->sendResponse(array(), trans('custom.successfully_export'));
 
-        $doc_name = 'purchase_order';
+        $doc_name = trans('custom.purchase_order');
         $doc_name_path = 'purchase_order/';
         if($input['documentId'] == 52)
         {
-            $doc_name = 'purchase_direct_order';
+            $doc_name = trans('custom.purchase_direct_order');
             $doc_name_path = 'purchase_direct_order/';
         }
         else if($input['documentId'] == 5)
         {
-            $doc_name = 'purchase_work_order';
+            $doc_name = trans('custom.purchase_work_order');
             $doc_name_path = 'purchase_work_order/';
         }
         $companyID = isset($input['companyId']) ? $input['companyId']: null;
