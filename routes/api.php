6ec4e02ce3c68e7573a822dac2d09e9c6b9ab480
Vishwa-Dhananjaya <138758509+Vishwa-Dhananjaya@users.noreply.github.com>
2025-12-08 21:39:32 +0530

    fix(accounts receivable): Fix Auto-Confirmation/Approval Issue for Receipt Voucher Created via API [GCP-18150]
    fix(accounts receivable): Fix Auto-Confirmation/Approval Issue for Receipt Voucher Created via API [GCP-18150]

--- routes/api.php
+++ routes/api.php
@@ -1056,8 +1056,10 @@ Route::group(['middleware' => ['mobileServer']], function () {
             Artisan::call($cron);
             return 'CRON Job run successfully';
         });
+        Route::get('confirmAPICreatedReceiptVouchers', 'ReceiptAPIController@confirmAPICreatedReceiptVouchers');
     }
 });
+
 
 
 
