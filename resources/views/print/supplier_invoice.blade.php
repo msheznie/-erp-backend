007672984750fea8eeadb350b354d6665650d35f
nilaf246 <89913141+nilaf246@users.noreply.github.com>
2025-12-30 08:29:16 +0530

    feat(accounts payable): Item Supplier Invoice WHT COnfiguration [GCP-17822] (#9495)

--- resources/views/print/supplier_invoice.blade.php
+++ resources/views/print/supplier_invoice.blade.php
@@ -901,7 +901,7 @@
                         style="font-size: 11px;border-left: 1px solid rgb(127, 127, 127) !important;border-right: 1px solid rgb(127, 127, 127) !important;">
                     <span class="font-weight-bold">
                         @if ($masterdata->detail)
-                            {{number_format((($subTotal + $VATTotal + $directTotTra) - $retentionVatPortion), $transDecimal)}}
+                            {{number_format((($subTotal + $VATTotal + $directTotTra) - ($masterdata->whtApplicable && $masterdata->whtPaymentMethod != 2 ? $masterdata->whtAmount : 0) - $retentionVatPortion), $transDecimal)}}
                         @endif
                     </span>
                     </td>
