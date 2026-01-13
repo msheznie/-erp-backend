84b6e1a33f9e7a42fc14aa111ec9a439964629c5
mudithamee <50987167+mudithamee@users.noreply.github.com>
2026-01-13 16:26:26 +0530

    feat(Tasking Request): Portal | EA | Task assignment approval following job procedure setup [GHR-9581] (#9569)
    * feat(Tasking Request): Portal | EA | Task assignment approval following job procedure setup [GHR-9581]
    
    * feat(Tasking Request): Portal | EA | Task assignment approval following job procedure setup-code reviwed changes [GHR-9581]
    
    * feat(Tasking Request): Portal | EA | Task assignment approval following job procedure setup-code reviwed changes [GHR-9581]
    
    * feat(Tasking Request): Portal | EA | Task assignment approval following job procedure setup-code reviwed changes [GHR-9581]
    
    ---------
    
    Co-authored-by: mudithamee <muditha@osos.om>

---
+++ app/Jobs/EmployeeTaskingNotificationJob.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\helper\CommonJobService;
+use App\Services\hrms\employee\EmployeeTaskingNotificationService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+
+class EmployeeTaskingNotificationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    public $companyId;
+    public $id;
+    public $dbName;
+    public $masterDetails;
+
+    public function __construct($dbName, $companyId, $id, $masterDetails)
+    {
+        if(env('IS_MULTI_TENANCY',false)){
+            self::onConnection('database_main');
+        }else{
+            self::onConnection('database');
+        }
+
+        $this->dbName = $dbName;
+        $this->companyId = $companyId;
+        $this->id = $id;
+        $this->masterDetails = $masterDetails;
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::useFiles( CommonJobService::get_specific_log_file('employee-tasking-notification') );
+
+        if (empty($this->dbName)) {
+            Log::error("db details not found. \t on file: " . __CLASS__ ." \tline no :".__LINE__);
+
+        } else {
+            
+            CommonJobService::db_switch($this->dbName);
+            $obj = new EmployeeTaskingNotificationService($this->companyId, $this->id, $this->masterDetails);
+            $obj->execute();
+        }
+    }
+}
+
