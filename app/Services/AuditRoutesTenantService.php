d6b27080b5b561d1503a470217e50fce40b20708
Zakeeul <shakeeul@osos.om>
2026-01-19 15:32:56 +0530

    chore: add logdetail route to teannt service

--- app/Services/AuditRoutesTenantService.php
+++ app/Services/AuditRoutesTenantService.php
@@ -112,6 +112,7 @@ class AuditRoutesTenantService
             'api/v1/userGroups/{userGroup}',
             'api/v1/employee_navigations',
             'api/v1/employee_navigations/{employee_navigation}',
+            'api/v1/getThirdPartyApiLogDetail'
         ];
 
         return $lokiTenantRoutes;
