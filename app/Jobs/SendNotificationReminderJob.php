64cd4e77bb7a418ecaf959d51658951683a80348
Ishfaq-A <61774663+Ishfaq-A@users.noreply.github.com>
2026-01-27 22:36:39 +0530

    chore(srm): not receiving reminder notification email [GSUP-5090] (#9707)

--- app/Jobs/SendNotificationReminderJob.php
+++ app/Jobs/SendNotificationReminderJob.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use App\helper\CommonJobService;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -42,9 +43,18 @@ class SendNotificationReminderJob implements ShouldQueue
      */
     public function handle()
     {
-        $db = $this->dispatchDB;
-        CommonJobService::db_switch($db);
-        $service = new SendTenderNotificationService();
-        $service->tenderNotificationScenarioBased();
+        try {
+            $db = $this->dispatchDB;
+            CommonJobService::db_switch($db);
+            $service = new SendTenderNotificationService();
+            $service->tenderNotificationScenarioBased();
+        } catch (\Exception $e) {
+            Log::error('SendNotificationReminderJob failed', [
+                'database' => $this->dispatchDB,
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
+            throw $e;
+        }
     }
 }
