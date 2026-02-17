eb8967659134da52b545a579731984594d7aa46f
Achini Athapaththu <achini.athapaththu>
2026-02-17 06:06:29 +0000

    feat(system admin): GET API | Customer master (Pull ) [GCP-19917]

--- routes/api.php
+++ routes/api.php
@@ -65,8 +65,6 @@ Route::group(['middleware' => ['mobileServer']], function () {
             Route::post('warehouse/items', 'ItemMasterAPIController@getWarehouseItemQuantity');
             Route::post('customer_master', 'CustomerMasterAPIController@pullCustomerMaster');
         });
-
-        Route::post('customer_master_pull', 'CustomerMasterAPIController@pullCustomerMaster');
         
         Route::post('updateDocumentCodeTransaction', 'DocumentCodeMasterAPIController@updateDocumentCodeTransaction')->middleware([ExtractHeadersFromBody::class,'auth.api.keycloak','authorization:api','mobileAccess']);
 
