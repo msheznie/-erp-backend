5e6b914743a583e5512855b6de613d5d0477a9db
Aflal Abdeen <aflal@gears-int.com>
2026-02-17 11:07:10 +0530

    fix(Tender): 500 Error in Bid Opening – Submitted Documents & View Icon Disabled [GSUP-5249]

--- app/Http/Controllers/API/TenderMasterAPIController.php
+++ app/Http/Controllers/API/TenderMasterAPIController.php
@@ -2659,6 +2659,7 @@ class TenderMasterAPIController extends AppBaseController
             if ($closing_commer_date_comp == null) {
                 $result2 = true;
             } else {
+                $closing_commer_date_comp = Carbon::parse($closing_commer_date_comp);
                 $result2 = $closing_commer_date_comp->gt($current_date2);
             }
 
