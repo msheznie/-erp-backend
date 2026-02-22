c6d2fb926cee06c3b4c797123956a2341174da0a
Zakeeul <shakeeul@osos.om>
2026-02-23 05:16:11 +0530

    chore: remove auto fix in authorization

--- app/Http/Middleware/UserAuthorization.php
+++ app/Http/Middleware/UserAuthorization.php
@@ -58,35 +58,35 @@ class UserAuthorization
             // $navigationID = $request->header('X-nav-ID') ?? 0;
             // $accessType = $request->header('X-Access-Type') ?? 'None';
 
-            if ($routeName != 'api.' && $navigationID > 0 && self::getActionType($accessType) > 0) {
+            // if ($routeName != 'api.' && $navigationID > 0 && self::getActionType($accessType) > 0) {
 
-                NavigationRoute::firstOrCreate(
-                    [
-                        'navigationID' => $navigationID,
-                        'routeName' => $routeName,
-                        'action' => self::getActionType($accessType),
-                    ]
-                );
+            //     NavigationRoute::firstOrCreate(
+            //         [
+            //             'navigationID' => $navigationID,
+            //             'routeName' => $routeName,
+            //             'action' => self::getActionType($accessType),
+            //         ]
+            //     );
 
-                foreach ($userGroupIDs as $userGroupID) {
-                    RoleRoute::create([
-                        'routeName' => $routeName,
-                        'userGroupID' => $userGroupID,
-                        'companySystemID' => 0
-                    ]);
-                }
+            //     foreach ($userGroupIDs as $userGroupID) {
+            //         RoleRoute::create([
+            //             'routeName' => $routeName,
+            //             'userGroupID' => $userGroupID,
+            //             'companySystemID' => 0
+            //         ]);
+            //     }
                 
 
-                $checkRoleRouteAfterCreate = RoleRoute::whereIn('userGroupID', $userGroupIDs)
-                                    ->where('routeName', $routeName)
-                                    ->first();
+            //     $checkRoleRouteAfterCreate = RoleRoute::whereIn('userGroupID', $userGroupIDs)
+            //                         ->where('routeName', $routeName)
+            //                         ->first();
 
-                if ($checkRoleRouteAfterCreate) {
-                    return $next($request);
-                } else {
-                    return errorMsgs("Unauthorized Access");
-                }
-            }
+            //     if ($checkRoleRouteAfterCreate) {
+            //         return $next($request);
+            //     } else {
+            //         return errorMsgs("Unauthorized Access");
+            //     }
+            // }
 
             // \Log::channel('authorization')->info(json_encode([
             //     'navigationID' => $navigationID,
