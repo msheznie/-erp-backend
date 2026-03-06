353260b1dad41a859e5dc7471464ab0e598ae295
Mohammed Arshad <m.arshad@osos.om>
2026-03-06 06:52:59 +0000

    feat(accounts payable): Changes in cheque print template [GCP-21407]

--- resources/views/print/bank_transfer.blade.php
+++ resources/views/print/bank_transfer.blade.php
@@ -224,8 +224,10 @@
                     and
                     {{$entity->floatAmt}}/@if($entity->decimalPlaces == 3)1000 @else 100 @endif] only as folllow and debit the same to 
                     our above mentioned account under advice to us , with a conversion to the beneficiary currency at the prevailing exchange rate.<br>
-
-                    <br><br>
+                    <br>
+                    All Bank charges to be debited from our account.
+                    <br>
+                    <br>
                     <b>Beneficiary Name         :  </b> @if($entity->memos){{isset($entity->memos[0]->memoDetail)?$entity->memos[0]->memoDetail:''}}@endif <br>
                     <b>Beneficiary Address      :  </b> @if($entity->supplier){{$entity->supplier->address}}@endif
                     <br><br>
