458f443d9161762c6dadcb2ce0625c58260c7274
Muditha Pushpani <muditha@osos.om>
2026-03-09 11:11:28 +0000

    feat(Employee movement): HRMS | EA | Job Procedures | Notification After Approval of Employee Movement Request Based on Movement Type & Classification [GHR-10959]

---
+++ app/Jobs/EmployeeMovementNotificationJob.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\helper\CommonJobService;
+use App\Services\hrms\employee\EmployeeMovementNotificationService;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+
+class EmployeeMovementNotificationJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    public $companyId;
+    public $id;
+    public $dbName;
+    public $masterDetails;
+
+    public function __construct($dbName, $companyId, $id, $masterDetails)
+    {
+        if (env('QUEUE_DRIVER_CHANGE','database') == 'database') {
+            if (env('IS_MULTI_TENANCY',false)) {
+                self::onConnection('database_main');
+            }
+            else {
+                self::onConnection('database');
+            }
+        }
+        else {
+            self::onConnection(env('QUEUE_DRIVER_CHANGE','database'));
+        }
+
+        $this->dbName = $dbName;
+        $this->companyId = $companyId;
+        $this->id = $id;
+        $this->masterDetails = $masterDetails;
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (empty($this->dbName)) {
+           Log::channel('employee_movement_notification')->error("db details not found. \t on file: " . __CLASS__ ." \tline no :".__LINE__);
+
+        } else {
+            CommonJobService::db_switch($this->dbName);
+            $obj = new EmployeeMovementNotificationService($this->companyId, $this->id, $this->masterDetails);
+            $obj->execute();
+        }
+    }
+}
