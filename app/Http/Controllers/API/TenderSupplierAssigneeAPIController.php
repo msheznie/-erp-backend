2b5096a6bff5deebe9b971460dd2d6c7095603dd
Ishfaq.ahamed <ishfaq@osos.om>
2026-03-12 21:13:11 +0530

    chore(srm): issue when resend email when tender invitation resend [GSUP-5736]

--- app/Http/Controllers/API/TenderSupplierAssigneeAPIController.php
+++ app/Http/Controllers/API/TenderSupplierAssigneeAPIController.php
@@ -446,10 +446,16 @@ class TenderSupplierAssigneeAPIController extends AppBaseController
         try {
             $token = md5(Carbon::now()->format('YmdHisu'));
             $loginUrl = env('SRM_LINK') . $token . '/' . $apiKey;
-            $name = data_get($val, 'supplierAssigned.supplierName') ?? $val['supplier_name'];
-            $email = data_get($val, 'supplierAssigned.supEmail') ?? $val['supplier_email'];
-            $regNo = data_get($val, 'supplierAssigned.registrationNumber') ?? $val['registration_number'];
-            $isBidTender = data_get($val, 'supplierAssigned.registrationNumber') !== null ? 0 : 1;
+            $name  = optional($getSupplierAssignedData->supplierAssigned)->supplierName
+                ?? $getSupplierAssignedData->supplier_name;
+
+            $email = optional($getSupplierAssignedData->supplierAssigned)->supEmail
+                ?? $getSupplierAssignedData->supplier_email;
+
+            $regNo = optional($getSupplierAssignedData->supplierAssigned)->registrationNumber
+                ?? $getSupplierAssignedData->registration_number;
+
+            $isBidTender = optional($getSupplierAssignedData->supplierAssigned)->registrationNumber ? 0 : 1;
             $isExist = SupplierRegistrationLink::select('id','STATUS', 'token')
                 ->where('company_id', $companySystemId)
                 ->where('email', $email)
