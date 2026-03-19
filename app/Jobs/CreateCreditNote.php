014b5ded7d5e177c3b4362215ab81a4103c5bcc9
vishwa.dhananjaya@osos.om <vishwa.dhananjaya@osos.om>
2026-03-19 07:01:41 +0000

    API changes

--- app/Jobs/CreateCreditNote.php
+++ app/Jobs/CreateCreditNote.php
@@ -153,7 +153,8 @@ class CreateCreditNote implements ShouldQueue
             $receiptVoucherIndex = 0;
             $receiptVoucherDetails = $creditNote['receipt_voucher_details'] ?? null;
 
-            if (!is_null($receiptVoucherDetails) && $datasetMaster['status']) {
+
+            if(!is_null($receiptVoucherDetails) && $datasetMaster['status']) {
                 foreach ($receiptVoucherDetails as $reciptVoucherDetail) {
                     $datasetReceiptVoucher = self::validateCNReceiptVoucherDetailsData($creditNote, $reciptVoucherDetail, $datasetMaster);
 
@@ -171,8 +172,9 @@ class CreateCreditNote implements ShouldQueue
                 }
             }
 
+
             // Validate refund total matches credit note line total for refund type credit note
-            if (!is_null($receiptVoucherDetails) && isset($receiptVoucherDataSets[$masterIndex])) {
+            if(!is_null($receiptVoucherDetails) && isset($receiptVoucherDataSets[$masterIndex]) && isset($detailsDataSets[$masterIndex])) {
                 $totalMatchValidation = self::validateRefundAmountWithCreditNoteAmount($detailsDataSets[$masterIndex], $receiptVoucherDataSets[$masterIndex]);
                 if (!$totalMatchValidation['status']) {
                     $headerData['errors'] = [
