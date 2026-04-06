bf4a4efd2734488816410557ff3b0048ef7d099a
saravanan <tharparanthan.saravanan@osos.om>
2026-04-06 07:01:38 +0000

    changes

--- app/Http/Controllers/API/ProcumentOrderAPIController.php
+++ app/Http/Controllers/API/ProcumentOrderAPIController.php
@@ -1422,7 +1422,7 @@ class ProcumentOrderAPIController extends AppBaseController
                                 "consumedLocalAmount" => $value->GRVcostPerUnitLocalCur,
                                 "consumedRptCurrencyID" => $value->companyReportingCurrencyID,
                                 "consumedRptAmount" => $value->GRVcostPerUnitComRptCur,
-                                'companyFinanceYearID' =>  CompanyFinanceYear::financeYearID($value->budgetYear, $value->companySystemID),
+                                "companyFinanceYearID" =>  CompanyFinanceYear::financeYearID($value->budgetYear, $value->companySystemID),
                                 "timestamp" => date('d/m/Y H:i:s A')
                             );
                         }
@@ -1450,7 +1450,7 @@ class ProcumentOrderAPIController extends AppBaseController
                                     "consumedLocalAmount" => $value->GRVcostPerUnitLocalCur,
                                     "consumedRptCurrencyID" => $value->companyReportingCurrencyID,
                                     "consumedRptAmount" => $value->GRVcostPerUnitComRptCur,
-                                    'companyFinanceYearID' =>  CompanyFinanceYear::financeYearID($value->budgetYear, $value->companySystemID),
+                                    "companyFinanceYearID" =>  CompanyFinanceYear::financeYearID($value->budgetYear, $value->companySystemID),
                                     "timestamp" => date('d/m/Y H:i:s A')
                                 );
                             }
