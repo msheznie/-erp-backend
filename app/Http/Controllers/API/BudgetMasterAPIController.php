1a67dddcc5f7f598412c0ffb70893a33556403f0
Thatparanthan Saravanan <thatparanthan.saravanan@osos.om>
2026-04-07 17:46:10 +0000

    feat(budget): manally closed po appears in commitment report fixes [GCP-21990]

--- app/Http/Controllers/API/BudgetMasterAPIController.php
+++ app/Http/Controllers/API/BudgetMasterAPIController.php
@@ -690,7 +690,6 @@ class BudgetMasterAPIController extends AppBaseController
                                 'erp_budjetdetails.chartOfAccountID', 'erp_budjetdetails.companyFinanceYearID'])
                             ->orderBy('erp_companyreporttemplatedetails.description','ASC')
                             ->get();
-                                
         foreach ($reportData as $key => $value) {
             $commitedConsumedAmount = BudgetConsumptionService::getCommitedConsumedAmount($value, $DLBCPolicy, true);
             $value['actuallConsumptionAmount'] = $commitedConsumedAmount['actuallConsumptionAmount'];
@@ -2889,7 +2888,8 @@ class BudgetMasterAPIController extends AppBaseController
                             ->where('serviceLineSystemID', $data['serviceLineSystemID'])
                             ->with(['purchase_order' => function ($query) use($data){
                                 
-                                $query->with(['detail','grv_details'=>function($query) use($data){
+                                $query->where('manuallyClosed', 0)
+                                    ->with(['detail','grv_details'=>function($query) use($data){
                                     $query->select('grvDetailsID','grvAutoID','purchaseOrderMastertID','purchaseOrderDetailsID','financeGLcodePLSystemID','netAmount')->with(['grv_master'=>function($query){
                                         $query->select('grvAutoID','grvPrimaryCode','approved','grvConfirmedYN','grvTotalComRptCurrency');
                                     }]);
@@ -2903,6 +2903,7 @@ class BudgetMasterAPIController extends AppBaseController
                             ->where('documentSystemID', 2)
                             ->when($data['controlAccountsSystemID'] != 3,function($query){
                                 $query->whereHas('purchase_order', function ($query) {
+                                  $query->where('manuallyClosed', 0);
                                   //$query->where('grvRecieved', '!=', 2);
                                 });
                             })
