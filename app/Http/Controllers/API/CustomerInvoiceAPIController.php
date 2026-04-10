7be26329b47ba03e1e484d500bd3cfb0eb555d59
Achini Athapaththu <achini.athapaththu>
2026-04-10 14:10:42 +0000

    chore(account receivable): User is not validated when requesting customer invoices without arrays for the invoice code [GCP-24127]

--- app/Http/Controllers/API/CustomerInvoiceAPIController.php
+++ app/Http/Controllers/API/CustomerInvoiceAPIController.php
@@ -334,8 +334,14 @@ class CustomerInvoiceAPIController extends AppBaseController
 
     public function getApprovedCustomerInvoiceBalancesAPI(GetApprovedCustomerInvoiceBalancesAPIRequest $request)
     {
-
+        
         $input = $request->all();
+        $contentLength = intval(request()->header('content-length'));
+
+        if(($input == [] || $input == null) && $contentLength > 0) {
+            return $this->sendError("Invalid request body", 400);
+        }
+
         $response = $this->customerInvoiceBalanceAPIService->getApprovedBalances($input);
         if (! $response->isSuccess()) {
             return $this->sendError($response->getMessage(), $response->getStatusCode());
