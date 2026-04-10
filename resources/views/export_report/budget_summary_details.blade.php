e61e01fbdc081672ca5b6fd5fcaf7a3877c52668
Achini Athapaththu <achini.athapaththu>
2026-04-10 21:18:48 +0000

    chore(budget): Budget Summary Report | JV Actual Column Not Updating [GCP-22099]

--- resources/views/export_report/budget_summary_details.blade.php
+++ resources/views/export_report/budget_summary_details.blade.php
@@ -12,18 +12,26 @@
         <td><strong>{{ trans('custom.gl_code') }}</strong></td>
         <td><strong>{{ trans('custom.document_code') }}</strong></td>
         <td><strong>{{ trans('custom.year') }}</strong></td>
-        <td><strong>{{ trans('custom.pending_amount') }}</strong></td>
+        <td><strong>{{ trans('custom.' . ($detailAmountLabelKey ?? 'pending_amount')) }}</strong></td>
     </tr>
     </thead>
     <tbody>
     @foreach($reportData as $item)
+        @php
+            $companyID = data_get($item, 'companyID');
+            $department = data_get($item, 'serviceLine', data_get($item, 'serviceLineCode'));
+            $glCode = data_get($item, 'financeGLcodePL', data_get($item, 'GLCode'));
+            $documentCode = data_get($item, 'documentCode');
+            $year = data_get($item, 'budgetYear', data_get($item, 'year'));
+            $amount = data_get($item, 'lineTotal', data_get($item, 'actualConsumption', data_get($item, 'committedAmount', data_get($item, 'consumedRptAmount', 0))));
+        @endphp
         <tr>
-            <td>{{ $item['companyID'] }}</td>
-            <td>{{ $item['serviceLine'] }}</td>
-            <td>{{ $item['financeGLcodePL'] }}</td>
-            <td>{{ $item['documentCode'] }}</td>
-            <td>{{ $item['budgetYear'] }}</td>
-            <td>{{ number_format($item['lineTotal'],2) }}</td>
+            <td>{{ $companyID }}</td>
+            <td>{{ $department }}</td>
+            <td>{{ $glCode }}</td>
+            <td>{{ $documentCode }}</td>
+            <td>{{ $year }}</td>
+            <td>{{ number_format((float) $amount, 2) }}</td>
 
         </tr>
     @endforeach
